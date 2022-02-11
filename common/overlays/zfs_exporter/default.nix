c846d3ba9579e5d23da1158f7fba49725c4f69d1
GGG KILLER <gggkiller2@gmail.com>
2022-02-11 01:19:29 +0000

    Fix zfs_exporter hash.

--- common/overlays/zfs_exporter/default.nix
+++ common/overlays/zfs_exporter/default.nix
@@ -8,7 +8,7 @@ buildGoModule rec {
     owner = "pdf";
     repo = "zfs_exporter";
     rev = "v${version}";
-    sha256 = "sha256-niLv3ohFJol/Qsa3+wX91tF7WEdK/wWO7kbYztVdM+E=";
+    sha256 = "sha256-FY3P2wmNWyr7mImc1PJs1G2Ae8rZvDzq0kRZfiRTzyc=";
   };
 
   vendorSha256 = "sha256-jQiw3HlqWcsjdadDdovCsDMBB3rnWtacfbtzDb5rc9c=";
