5e46c6ce216842e78e191d66dce0bd5182405b3f
GGG KILLER <gggkiller2@gmail.com>
2022-03-08 16:24:35 +0000

    Add the monit service.

---
+++ hosts/shiro/services/monitoring/monit.nix
@@ -0,0 +1,25 @@
+{ lib, ... }:
+
+with lib;
+let
+  port = 9925;
+in
+{
+  services.monit = {
+    enable = true;
+    config = ''
+      # Set the daemon poll time
+      SET DAEMON 30
+
+      # Set up the HTTP Port
+      SET HTTPD PORT ${toString port}
+          ALLOW 127.0.0.1
+    '';
+  };
+
+  services.nginx.virtualHosts."monit.shiro.lan" = {
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString port}";
+    };
+  };
+}
