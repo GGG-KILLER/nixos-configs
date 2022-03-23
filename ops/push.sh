cd752ca7983313cba4f6cef7ce61033a5b4f47a2
GGG KILLER <gggkiller2@gmail.com>
2022-03-23 18:34:05 +0000

    Accept args so I can use filters.

--- ops/push.sh
+++ ops/push.sh
@@ -5,6 +5,6 @@ set -e
 network="$(dirname "${BASH_SOURCE[0]}")/network.nix"
 ident_file="$(dirname "${BASH_SOURCE[0]}")/deploy.privkey"
 
-morph build --keep-result "$network"
-SSH_IDENTITY_FILE="$ident_file" morph push "$network"
-SSH_IDENTITY_FILE="$ident_file" morph deploy "$network" switch
+morph build --keep-result "$network" $*
+SSH_IDENTITY_FILE="$ident_file" morph push "$network" $*
+SSH_IDENTITY_FILE="$ident_file" morph deploy "$network" switch $*
