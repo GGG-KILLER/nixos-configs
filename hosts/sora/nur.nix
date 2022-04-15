bb77d3bd8f2615e45573be810ffd9deeea0f8356
GGG <gggkiller2@gmail.com>
2022-04-15 20:30:12 -0300

    Simplify the morph config.

--- hosts/sora/nur.nix
+++ hosts/sora/nur.nix
@@ -6,11 +6,8 @@
       inherit pkgs;
     };
     rnix-lsp = import (builtins.fetchTarball "https://github.com/nix-community/rnix-lsp/archive/master.tar.gz");
-    morph = pkgs.callPackage (pkgs.fetchFromGitHub {
-      owner = "DBCDK";
-      repo = "morph";
-      rev = "master";
-      hash = "sha256-0CHmjqPxBgALGZYjfJFLoLBnoI0U7oZ8WyCtu1bkzZg=";
-    }) { };
+    morph = import (builtins.fetchTarball "https://github.com/DBCDK/morph/archive/refs/heads/master.zip") {
+      inherit pkgs;
+    };
   };
 }
