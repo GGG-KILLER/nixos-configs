157129e273182eddc3df30fd97f324a18cb9c3a9
GGG <gggkiller2@gmail.com>
2022-04-17 01:03:39 -0300

    Update ggg config.

--- hosts/sora/users/ggg.nix
+++ hosts/sora/users/ggg.nix
@@ -1,14 +1,41 @@
-{ pkgs, rnix-lsp, ... }:
+{ pkgs, ... }:
 
+let
+  dotnet-sdk = pkgs: (with pkgs.dotnetCorePackages; combinePackages [
+    sdk_6_0
+    sdk_5_0
+    sdk_3_1
+  ]);
+in
 {
-  users.users.ggg.packages = with pkgs; [
-    powershell
-    helvum
-    virt-manager
-    vscode
-    rnix-lsp
-    morph
-  ];
+  home-manager.users.ggg = {
+    home.packages = with pkgs; [
+      powershell
+      helvum
+      virt-manager
+      rnix-lsp
+      morph
+      (dotnet-sdk pkgs)
+      mono
+    ];
+
+    programs = {
+      home-manager.enable = true;
+      vscode = {
+        enable = true;
+        package = pkgs.vscode-fhsWithPackages (pkgs: with pkgs; [
+          rnix-lsp
+          (dotnet-sdk pkgs)
+          mono
+        ]);
+      };
+      git = {
+        enable = true;
+        userName = "GGG";
+        userEmail = "gggkiller2@gmail.com";
+      };
+    };
+  };
 
   modules.home.mainUsers = [ "ggg" ];
 }
