e3194e10855fe9cc31c0e09b2d316164a7bbf799
GGG <gggkiller2@gmail.com>
2022-04-22 18:09:33 -0300

    Remove morph.

--- hosts/sora/overrides.nix
+++ hosts/sora/overrides.nix
@@ -2,8 +2,5 @@
 
 {
   nixpkgs.config.packageOverrides = pkgs: {
-    morph = import (builtins.fetchTarball "https://github.com/DBCDK/morph/archive/refs/heads/master.zip") {
-      inherit pkgs;
-    };
   };
 }
