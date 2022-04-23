5bf3a550e39f3e3db72f5ab847e7e79ebb717ad1
GGG <gggkiller2@gmail.com>
2022-04-23 17:02:49 -0300

    Update xorg config.

--- hosts/sora/video.nix
+++ hosts/sora/video.nix
@@ -21,12 +21,17 @@
   };
 
   services.xserver.screenSection = ''
+    Option         "Stereo" "0"
     Option         "nvidiaXineramaInfoOrder" "DFP-3"
-    Option         "metamodes" "DP-2: nvidia-auto-select +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, HDMI-1: nvidia-auto-select +1920+28 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}"
+    Option         "metamodes" "DP-2: 1920x1080_144 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, HDMI-1: 1920x1080_75 +1920+28 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}"
     Option         "SLI" "Off"
     Option         "MultiGPU" "Off"
     Option         "BaseMosaic" "off"
     Option         "AllowIndirectGLXProtocol" "off"
     Option         "TripleBuffer" "on"
+    SubSection     "Display"
+      Depth        24
+      Modes        "1920x1080" "1366x768" "1280x720"
+    EndSubSection
   '';
 }
