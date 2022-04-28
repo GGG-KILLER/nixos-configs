a322997785cf9edb30c126e8ff0b28595513fef6
GGG <gggkiller2@gmail.com>
2022-04-28 16:57:01 -0300

    Create the firefly-iii container.

---
+++ hosts/shiro/containers/firefly-iii.nix
@@ -0,0 +1,78 @@
+{ config, lib, pkgs, ... }@args:
+
+with lib;
+let
+  inherit (import ./funcs.nix args) mkContainer;
+  consts = config.my.constants;
+in
+rec {
+  my.networking.firefly-iii = {
+    ipAddrs = {
+      elan = "192.168.1.9";
+      # clan = "192.168.2.13";
+    };
+    ports = [
+      {
+        protocol = "http";
+        port = 80;
+        description = "Local Nginx";
+      }
+    ];
+  };
+
+  containers.firefly-iii = mkContainer {
+    name = "firefly-iii";
+
+    includeAnimu = false;
+    includeEtc = false;
+    includeH = false;
+
+    bindMounts = {
+      "/var/www/firefly-iii" = {
+        hostPath = "/zfs-main-pool/data/firefly-iii";
+        isReadOnly = false;
+      };
+    };
+
+    config = { config, pkgs, ... }:
+      {
+        services.phpfpm.pools.mypool = {
+          user = "nobody";
+          phpPackage = (pkgs.php.buildEnv {
+            extensions = ({ enabled, all }: enabled ++ (with all;[
+              bcmath
+              intl
+              curl
+              zip
+              sodium
+              gd
+              xml
+              mbstring
+              pgsql
+              pdo_pgsql
+            ]));
+          });
+          settings = {
+            "pm" = "dynamic";
+            "listen.owner" = config.services.nginx.user;
+            "pm.max_children" = 5;
+            "pm.start_servers" = 2;
+            "pm.min_spare_servers" = 1;
+            "pm.max_spare_servers" = 3;
+            "pm.max_requests" = 500;
+          };
+        };
+
+        services.nginx = {
+          enable = true;
+          virtualHosts."money.lan" = {
+            root = "/var/www/firefly-iii";
+            locations."~ \.php$".extraConfig = ''
+              fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+              fastcgi_index index.php;
+            '';
+          };
+        };
+      };
+  };
+}
