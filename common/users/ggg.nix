c43e3f72635fce4c15b43f1a2b6a60d68b114553
GGG <gggkiller2@gmail.com>
2022-04-28 17:27:15 -0300

    Add ggg to nginx group.

--- common/users/ggg.nix
+++ common/users/ggg.nix
@@ -5,7 +5,7 @@
     uid = 1000;
     isNormalUser = true;
     description = "GGG";
-    extraGroups = [ "wheel" "data-members" "grafana" "prometheus" "libvirtd" ];
+    extraGroups = [ "wheel" "data-members" "grafana" "prometheus" "libvirtd" "nginx" ];
     hashedPassword = config.my.secrets.users.ggg.hashedPassword;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGIbyyT77P4fzRh4Bfox1GQANs+P5VTrVADu5+k282fn ggg@home-server"
