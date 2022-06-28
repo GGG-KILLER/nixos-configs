8d9fd9c0efef862ae2c2f79a78b0111e19d5d64d
GGG <gggkiller2@gmail.com>
2022-06-28 18:28:58 -0300

    Readd the latest tag and make it always pull.

--- hosts/shiro/containers/downloader.nix
+++ hosts/shiro/containers/downloader.nix
@@ -2,7 +2,7 @@
 
 {
   virtualisation.oci-containers.containers.downloader-frontend = {
-    image = "shiro.lan:5000/downloader/frontend:1";
+    image = "shiro.lan:5000/downloader/frontend:latest";
     ports = [ "9000:8080" ];
     dependsOn = [ "downloader-backend" ];
     extraOptions = [
@@ -11,16 +11,18 @@
       "--ipc=none"
       "--network=downloader"
       "--network-alias=frontend"
+      "--pull=always"
     ];
   };
 
   virtualisation.oci-containers.containers.downloader-backend = {
-    image = "shiro.lan:5000/downloader/backend:1";
+    image = "shiro.lan:5000/downloader/backend:latest";
     volumes = [
       "pgo:/app/PGO"
       "/zfs-main-pool/data/animu:/mnt/animu"
       "/zfs-main-pool/data/h:/mnt/h"
       "/zfs-main-pool/data/etc:/mnt/etc"
+      "--pull=always"
     ];
     environment = {
       ConnectionStrings__Main = config.my.secrets.downloader.connection-string;
