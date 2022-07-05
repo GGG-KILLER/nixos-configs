2ce6c3bdfadb98cb3dad7d3a698b89df8eda8d12
GGG <gggkiller2@gmail.com>
2022-07-05 17:33:47 -0300

    Remove comment from key.

--- common/users/ggg.nix
+++ common/users/ggg.nix
@@ -8,7 +8,7 @@
     extraGroups = [ "wheel" "data-members" "grafana" "prometheus" "libvirtd" "nginx" "docker" ];
     hashedPassword = config.my.secrets.users.ggg.hashedPassword;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGIbyyT77P4fzRh4Bfox1GQANs+P5VTrVADu5+k282fn ggg@home-server"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGIbyyT77P4fzRh4Bfox1GQANs+P5VTrVADu5+k282fn ggg"
     ];
     packages = with pkgs; [ man git netcat tcpdump htop nmon restic ];
   };
