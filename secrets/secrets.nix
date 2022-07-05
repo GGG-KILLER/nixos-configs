ec9751bccc6664b7c74130382c655c38cbfe4934
GGG <gggkiller2@gmail.com>
2022-07-05 17:08:31 -0300

    More secrets.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,7 +4,8 @@ let
 
   sora = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB6b2z/jMnPSYXSYYJ6NBY77m0bofpVceoArRzJHQ+Nc";
   shiro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYyYTusgW/GPy8qYBaS4gq71MEGWEY+U+m7rSUzn/xc";
-  systems = [ sora shiro ];
+  vpn-proxy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXJsB3idnONLnk1X/Ga2V0HHCJhjqptMbvdZVJji3F/";
+  systems = [ sora shiro vpn-proxy ];
 
   all = users ++ systems;
 in
@@ -15,9 +16,21 @@ in
   # Shiro
   "shiro/backup_password".publicKeys = [ ggg shiro ];
 
+  # Shiro - Wireguard
+  "shiro/wireguard/private_key".publicKeys = [ ggg shiro ];
+  "shiro/wireguard/laptop_psk".publicKeys = [ ggg shiro ];
+  "shiro/wireguard/phone_psk".publicKeys = [ ggg shiro ];
+
   # Shiro - StepCA
   "shiro/stepca/intermediate_ca_key".publicKeys = [ ggg shiro ];
   "shiro/stepca/intermediate_ca.crt".publicKeys = [ ggg shiro ];
   "shiro/stepca/root_ca_key".publicKeys = [ ggg shiro ];
   "shiro/stepca/root_ca.crt".publicKeys = [ ggg shiro ];
+
+  # VPN Proxy
+  "vpn-proxy/wireguard/private_key".publicKeys = [ ggg vpn-proxy ];
+  "vpn-proxy/wireguard/wing_psk".publicKeys = [ ggg vpn-proxy ];
+  "vpn-proxy/wireguard/ggg_psk".publicKeys = [ ggg vpn-proxy ];
+  "vpn-proxy/wireguard/spar_ios_psk".publicKeys = [ ggg vpn-proxy ];
+  "vpn-proxy/wireguard/spar_pc1_psk".publicKeys = [ ggg vpn-proxy ];
 }
