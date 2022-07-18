3a214da923c2a79102bdae9229a712f73dc388bd
GGG <gggkiller2@gmail.com>
2022-07-18 16:18:23 -0300

    Store json files in memory instead of on disk.

--- common/overlays/gen-dotnet-overlay.sh
+++ common/overlays/gen-dotnet-overlay.sh
@@ -2,20 +2,6 @@
 #! nix-shell -i bash -p curl jq
 set -eu
 
-RELEASES_INDEX_FILE=""
-VERSION_RELEASES_JSON_FILE=""
-function cleanup() {
-    [ ! \( -z "$RELEASES_INDEX_FILE" \) ] && rm "$RELEASES_INDEX_FILE";
-    [ ! \( -z "$VERSION_RELEASES_JSON_FILE" \) ] && rm "$VERSION_RELEASES_JSON_FILE";
-}
-function usage() {
-    echo "Usage: $0 [version]" >&2;
-    echo >&2;
-    printf "\tWhere version is a .NET version (e.g.: 6.0, 7.0).\n" >&2
-    printf "\tFile will be output to dotnet-\$VERSION.nix\n" >&2
-}
-trap cleanup EXIT
-
 if [[ $# != 1 ]]; then
     usage
     echo "Error: No arguments provided." >&2
@@ -29,60 +15,53 @@ if [[ ! ( $WANTED_VERSION =~ ^[[:digit:]]+\.[[:digit:]]+$ ) ]]; then
     echo "Error: Provided version was in an unrecognized format." >&2
     echo "       Expected version in format MAJOR.MINOR (e.g.: 7.0)." >&2
     exit 1
-fi;
+fi
 
 if [[ "$WANTED_VERSION" == "--help" || "$WANTED_VERSION" == "-h" ]]; then
     usage
     exit 1
 fi
 
-RELEASES_INDEX_FILE=$(mktemp dotnet-releases-index-XXXXXX.json)
-curl -s "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json" -o "$RELEASES_INDEX_FILE" || (
-    echo "Error fetching releases-index.json"
-    exit 1
-)
-VERSION_RELEASES_JSON_URL=$(jq -r ".[\"releases-index\"][] | select(.[\"channel-version\"] == \"$WANTED_VERSION\") | .[\"releases.json\"]" "$RELEASES_INDEX_FILE")
+RELEASES_INDEX=$(curl -sL "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json")
+VERSION_RELEASES_JSON_URL=$(jq -r ".[\"releases-index\"][] | select(.[\"channel-version\"] == \"$WANTED_VERSION\") | .[\"releases.json\"]" <<< "$RELEASES_INDEX")
 
 if [ -z "$VERSION_RELEASES_JSON_URL" ]; then
     echo "Version not found in releases."
     exit 1
 fi
 
-VERSION_RELEASES_JSON_FILE=$(mktemp "dotnet-$WANTED_VERSION-releases-XXXXXX.json")
-curl -s "$VERSION_RELEASES_JSON_URL" -o "$VERSION_RELEASES_JSON_FILE" || (
-    echo "Error downloading releases.json"
-    exit 1
-)
+VERSION_RELEASES_JSON=$(curl -sL "$VERSION_RELEASES_JSON_URL")
+LATEST_RELEASE=$(jq '.releases[0]' <<< "$VERSION_RELEASES_JSON")
 
-ASPNETCORE_VERSION=$(jq -r '.releases[0]["aspnetcore-runtime"].version' "$VERSION_RELEASES_JSON_FILE")
-ASPNETCORE_LINUX_X86_64_URL=$(jq -r '.releases[0]["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-linux-x64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-ASPNETCORE_LINUX_X86_64_HASH=$(jq -r '.releases[0]["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-linux-x64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-ASPNETCORE_LINUX_AARCH64_URL=$(jq -r '.releases[0]["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-linux-arm64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-ASPNETCORE_LINUX_AARCH64_HASH=$(jq -r '.releases[0]["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-linux-arm64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-ASPNETCORE_DARWIN_X86_64_URL=$(jq -r '.releases[0]["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-osx-x64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-ASPNETCORE_DARWIN_X86_64_HASH=$(jq -r '.releases[0]["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-osx-x64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-ASPNETCORE_DARWIN_AARCH64_URL=$(jq -r '.releases[0]["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-osx-arm64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-ASPNETCORE_DARWIN_AARCH64_HASH=$(jq -r '.releases[0]["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-osx-arm64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
+ASPNETCORE_VERSION=$(jq -r '.["aspnetcore-runtime"].version' <<< "$LATEST_RELEASE")
+ASPNETCORE_LINUX_X86_64_URL=$(jq -r '.["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-linux-x64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+ASPNETCORE_LINUX_X86_64_HASH=$(jq -r '.["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-linux-x64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+ASPNETCORE_LINUX_AARCH64_URL=$(jq -r '.["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-linux-arm64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+ASPNETCORE_LINUX_AARCH64_HASH=$(jq -r '.["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-linux-arm64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+ASPNETCORE_DARWIN_X86_64_URL=$(jq -r '.["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-osx-x64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+ASPNETCORE_DARWIN_X86_64_HASH=$(jq -r '.["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-osx-x64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+ASPNETCORE_DARWIN_AARCH64_URL=$(jq -r '.["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-osx-arm64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+ASPNETCORE_DARWIN_AARCH64_HASH=$(jq -r '.["aspnetcore-runtime"].files[] | select(.name == "aspnetcore-runtime-osx-arm64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
 
-RUNTIME_VERSION=$(jq -r '.releases[0].runtime.version' "$VERSION_RELEASES_JSON_FILE")
-RUNTIME_LINUX_X86_64_URL=$(jq -r '.releases[0].runtime.files[] | select(.name == "dotnet-runtime-linux-x64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-RUNTIME_LINUX_X86_64_HASH=$(jq -r '.releases[0].runtime.files[] | select(.name == "dotnet-runtime-linux-x64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-RUNTIME_LINUX_AARCH64_URL=$(jq -r '.releases[0].runtime.files[] | select(.name == "dotnet-runtime-linux-arm64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-RUNTIME_LINUX_AARCH64_HASH=$(jq -r '.releases[0].runtime.files[] | select(.name == "dotnet-runtime-linux-arm64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-RUNTIME_DARWIN_X86_64_URL=$(jq -r '.releases[0].runtime.files[] | select(.name == "dotnet-runtime-osx-x64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-RUNTIME_DARWIN_X86_64_HASH=$(jq -r '.releases[0].runtime.files[] | select(.name == "dotnet-runtime-osx-x64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-RUNTIME_DARWIN_AARCH64_URL=$(jq -r '.releases[0].runtime.files[] | select(.name == "dotnet-runtime-osx-arm64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-RUNTIME_DARWIN_AARCH64_HASH=$(jq -r '.releases[0].runtime.files[] | select(.name == "dotnet-runtime-osx-arm64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
+RUNTIME_VERSION=$(jq -r '.runtime.version' <<< "$LATEST_RELEASE")
+RUNTIME_LINUX_X86_64_URL=$(jq -r '.runtime.files[] | select(.name == "dotnet-runtime-linux-x64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+RUNTIME_LINUX_X86_64_HASH=$(jq -r '.runtime.files[] | select(.name == "dotnet-runtime-linux-x64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+RUNTIME_LINUX_AARCH64_URL=$(jq -r '.runtime.files[] | select(.name == "dotnet-runtime-linux-arm64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+RUNTIME_LINUX_AARCH64_HASH=$(jq -r '.runtime.files[] | select(.name == "dotnet-runtime-linux-arm64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+RUNTIME_DARWIN_X86_64_URL=$(jq -r '.runtime.files[] | select(.name == "dotnet-runtime-osx-x64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+RUNTIME_DARWIN_X86_64_HASH=$(jq -r '.runtime.files[] | select(.name == "dotnet-runtime-osx-x64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+RUNTIME_DARWIN_AARCH64_URL=$(jq -r '.runtime.files[] | select(.name == "dotnet-runtime-osx-arm64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+RUNTIME_DARWIN_AARCH64_HASH=$(jq -r '.runtime.files[] | select(.name == "dotnet-runtime-osx-arm64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
 
-SDK_VERSION=$(jq -r '.releases[0].sdk.version' "$VERSION_RELEASES_JSON_FILE")
-SDK_LINUX_X86_64_URL=$(jq -r '.releases[0].sdk.files[] | select(.name == "dotnet-sdk-linux-x64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-SDK_LINUX_X86_64_HASH=$(jq -r '.releases[0].sdk.files[] | select(.name == "dotnet-sdk-linux-x64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-SDK_LINUX_AARCH64_URL=$(jq -r '.releases[0].sdk.files[] | select(.name == "dotnet-sdk-linux-arm64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-SDK_LINUX_AARCH64_HASH=$(jq -r '.releases[0].sdk.files[] | select(.name == "dotnet-sdk-linux-arm64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-SDK_DARWIN_X86_64_URL=$(jq -r '.releases[0].sdk.files[] | select(.name == "dotnet-sdk-osx-x64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-SDK_DARWIN_X86_64_HASH=$(jq -r '.releases[0].sdk.files[] | select(.name == "dotnet-sdk-osx-x64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
-SDK_DARWIN_AARCH64_URL=$(jq -r '.releases[0].sdk.files[] | select(.name == "dotnet-sdk-osx-arm64.tar.gz") | .url' "$VERSION_RELEASES_JSON_FILE")
-SDK_DARWIN_AARCH64_HASH=$(jq -r '.releases[0].sdk.files[] | select(.name == "dotnet-sdk-osx-arm64.tar.gz") | .hash' "$VERSION_RELEASES_JSON_FILE")
+SDK_VERSION=$(jq -r '.sdk.version' <<< "$LATEST_RELEASE")
+SDK_LINUX_X86_64_URL=$(jq -r '.sdk.files[] | select(.name == "dotnet-sdk-linux-x64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+SDK_LINUX_X86_64_HASH=$(jq -r '.sdk.files[] | select(.name == "dotnet-sdk-linux-x64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+SDK_LINUX_AARCH64_URL=$(jq -r '.sdk.files[] | select(.name == "dotnet-sdk-linux-arm64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+SDK_LINUX_AARCH64_HASH=$(jq -r '.sdk.files[] | select(.name == "dotnet-sdk-linux-arm64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+SDK_DARWIN_X86_64_URL=$(jq -r '.sdk.files[] | select(.name == "dotnet-sdk-osx-x64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+SDK_DARWIN_X86_64_HASH=$(jq -r '.sdk.files[] | select(.name == "dotnet-sdk-osx-x64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
+SDK_DARWIN_AARCH64_URL=$(jq -r '.sdk.files[] | select(.name == "dotnet-sdk-osx-arm64.tar.gz") | .url' <<< "$LATEST_RELEASE")
+SDK_DARWIN_AARCH64_HASH=$(jq -r '.sdk.files[] | select(.name == "dotnet-sdk-osx-arm64.tar.gz") | .hash' <<< "$LATEST_RELEASE")
 
 FORMATTED_VERSION=${WANTED_VERSION//./_}
 
