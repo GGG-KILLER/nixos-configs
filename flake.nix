537e22033de7bfa9de401bd79cd90f52d146d85e
GGG <gggkiller2@gmail.com>
2022-07-20 14:07:14 -0300

    Update the stable nixpkgs.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    nixpkgs-stable.url = github:nixos/nixpkgs/nixos-21.11;
+    nixpkgs-stable.url = github:nixos/nixpkgs/nixos-22.05;
     nur.url = github:nix-community/NUR;
     home-manager = {
       url = github:nix-community/home-manager;
@@ -27,25 +27,37 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, nur, home-manager, deploy-rs, agenix, git-crypt-agessh, alejandra } @ inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      nurPkgs = system: import nur {
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-stable,
+    nur,
+    home-manager,
+    deploy-rs,
+    agenix,
+    git-crypt-agessh,
+    alejandra,
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    nurPkgs = system:
+      import nur {
         pkgs = pkgs;
         nurpkgs = pkgs;
       };
-      nurNoPkgs = system: import nur {
+    nurNoPkgs = system:
+      import nur {
         nurpkgs = pkgs;
       };
-      my-lib = pkgs.callPackage ./lib { };
-      mkConfig = host: nixpkgs.lib.nixosSystem {
+    my-lib = pkgs.callPackage ./lib {};
+    mkConfig = host:
+      nixpkgs.lib.nixosSystem {
         inherit system;
 
         specialArgs = {
           inherit system inputs nixpkgs nixpkgs-stable home-manager deploy-rs my-lib agenix git-crypt-agessh alejandra;
-          nur = (nurPkgs system);
-          nur-no-pkgs = (nurNoPkgs system);
+          nur = nurPkgs system;
+          nur-no-pkgs = nurNoPkgs system;
         };
 
         modules = [
@@ -54,38 +66,37 @@
           agenix.nixosModule
         ];
       };
-    in
-    {
-      nixosConfigurations = {
-        sora = mkConfig "sora";
-        shiro = mkConfig "shiro";
-        vpn-proxy = mkConfig "vpn-proxy";
-      };
-
-      deploy.nodes = {
-        shiro = {
-          hostname = "shiro.lan";
-          fastConnection = false;
-          profiles.system = {
-            user = "root";
-            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.shiro;
-            sshUser = "root";
-            autoRollback = false;
-            magicRollback = false;
-          };
-        };
-        vpn-proxy = {
-          hostname = "vpn-proxy.ggg.dev";
-          fastConnection = false;
-          profiles.system = {
-            user = "root";
-            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.vpn-proxy;
-            sshOpts = [ "-p" "17606" ];
-            sshUser = "root";
-          };
-        };
-      };
-
-      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+  in {
+    nixosConfigurations = {
+      sora = mkConfig "sora";
+      shiro = mkConfig "shiro";
+      vpn-proxy = mkConfig "vpn-proxy";
     };
+
+    deploy.nodes = {
+      shiro = {
+        hostname = "shiro.lan";
+        fastConnection = false;
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.shiro;
+          sshUser = "root";
+          autoRollback = false;
+          magicRollback = false;
+        };
+      };
+      vpn-proxy = {
+        hostname = "vpn-proxy.ggg.dev";
+        fastConnection = false;
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.vpn-proxy;
+          sshOpts = ["-p" "17606"];
+          sshUser = "root";
+        };
+      };
+    };
+
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+  };
 }
