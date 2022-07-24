f3cff13b5f1ed4e838a1538f9a00639aa4a3d20e
GGG <gggkiller2@gmail.com>
2022-07-24 15:30:08 -0300

    Make edit.sh point to the secrets file in the same dir as itself.

--- secrets/edit.sh
+++ secrets/edit.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p
 # shellcheck shell=bash
-EDITOR="code --wait" agenix -e "$@"
+ROOT_DIR="$(dirname "$(readlink -f "$0")")"
+RULES="$ROOT_DIR/secrets.nix" EDITOR="code --wait" agenix -e "$@"
