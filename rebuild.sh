4a148f9af536a206c57e2560cc10317ebdd32671
GGG <gggkiller2@gmail.com>
2022-07-23 23:38:00 -0300

    Add --flake to the rebuild script.

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p
 # shellcheck shell=bash
-nixos-rebuild --use-remote-sudo "$@"
+ROOT_DIR="$(dirname "$(readlink -f "$0")")"
+nixos-rebuild --use-remote-sudo --flake "$ROOT_DIR" "$@"
