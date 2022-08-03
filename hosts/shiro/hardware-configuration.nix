463fdc02e495c67a4552c900de4d6a2134eccc41
GGG <gggkiller2@gmail.com>
2022-08-03 18:58:44 -0300

    Remove fucked mounts.

--- hosts/shiro/hardware-configuration.nix
+++ hosts/shiro/hardware-configuration.nix
@@ -4,13 +4,7 @@
   pkgs,
   modulesPath,
   ...
-}: let
-  zmount = name: {
-    device = name;
-    fsType = "zfs";
-    options = ["zfsutil" "nofail"];
-  };
-in {
+}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
@@ -28,30 +22,6 @@ in {
 
   fileSystems."/nix" = {
     device = "zfs-main-pool/system/nix";
-    fsType = "zfs";
-    options = ["zfsutil"];
-  };
-
-  fileSystems."/var" = {
-    device = "zfs-main-pool/system/var";
-    fsType = "zfs";
-    options = ["zfsutil"];
-  };
-
-  fileSystems."/var/lib/grafana" = {
-    device = "zfs-main-pool/data/monitoring/grafana";
-    fsType = "zfs";
-    options = ["zfsutil"];
-  };
-
-  fileSystems."/var/lib/prometheus2" = {
-    device = "zfs-main-pool/data/monitoring/prometheus";
-    fsType = "zfs";
-    options = ["zfsutil"];
-  };
-
-  fileSystems."/var/lib/docker" = {
-    device = "zfs-main-pool/system/var/lib/docker";
     fsType = "zfs";
     options = ["zfsutil"];
   };
