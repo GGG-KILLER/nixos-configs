571b642ab098780644ce887a68d68828431322f4
GGG <gggkiller2@gmail.com>
2022-08-06 15:15:35 -0300

    Remove unnecessary insecure registry file.

--- hosts/shiro/virtualisation.nix
+++ hosts/shiro/virtualisation.nix
@@ -3,10 +3,5 @@
     enable = true;
     storageDriver = "zfs";
     autoPrune.enable = true;
-    daemon.settings = {
-      insecure-registries = [
-        "shiro.lan:5000"
-      ];
-    };
   };
 }
