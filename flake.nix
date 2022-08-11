0dd767a1d531565b3a6b82e233337c06eea67c9f
GGG <gggkiller2@gmail.com>
2022-08-11 15:14:27 -0300

    Update flake.

--- flake.nix
+++ flake.nix
@@ -2,30 +2,30 @@
   description = "GGG NixOS configuration";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    nixpkgs-stable.url = github:nixos/nixpkgs/nixos-22.05;
-    nur.url = github:nix-community/NUR;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.05";
+    nur.url = "github:nix-community/NUR";
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     deploy-rs = {
-      url = github:serokell/deploy-rs;
+      url = "github:serokell/deploy-rs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
-      url = github:ryantm/agenix;
+      url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     git-crypt-agessh = {
-      url = github:mtoohey31/git-crypt-agessh;
+      url = "github:mtoohey31/git-crypt-agessh";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     alejandra = {
-      url = github:kamadorueda/alejandra/2.0.0;
+      url = "github:kamadorueda/alejandra/2.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-index-database.url = "github:mic92/nix-index-database";
+    nix-index-database.url = github:mic92/nix-index-database;
   };
 
   outputs = {
@@ -49,9 +49,9 @@
         };
 
         modules = [
+          agenix.nixosModule
           ./common
           ./hosts/${host}/configuration.nix
-          agenix.nixosModule
         ];
       };
   in {
