7cd25e11fffb9d730d8c6cc05378a8892dff65bb
GGG <gggkiller2@gmail.com>
2022-10-12 15:08:55 -0300

    Fix dotnetCorePackages.combinePackages.

--- common/overlays/default.nix
+++ common/overlays/default.nix
@@ -1,7 +1,7 @@
 {...}: {
   imports = [
     #./dotnet-7.0.nix
-    #./dotnet-combine.nix
+    ./dotnet-combine.nix
     ./local.nix
     #./ms-dotnettools-csharp.nix
     ./prometheus-node-exporter.nix
