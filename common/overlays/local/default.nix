816f9c1f5cf987e70381a55e0ab637f21665bdb2
GGG <gggkiller2@gmail.com>
2022-10-15 15:27:31 -0300

    Fix local npm name.

--- common/overlays/local/default.nix
+++ common/overlays/local/default.nix
@@ -14,7 +14,7 @@
         zfs_exporter = callPackage ./zfs_exporter {};
         git-credential-manager = callPackage ./git-credential-manager {};
         winfonts = callPackage ./winfonts {};
-        node = callPackage ./npm {inherit (super) nodejs;};
+        npm = callPackage ./npm {inherit (super) nodejs;};
       };
     })
   ];
