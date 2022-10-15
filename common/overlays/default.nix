a7d57aeaceecf4a6c85284681ab450c5489af7f2
GGG <gggkiller2@gmail.com>
2022-10-15 14:57:22 -0300

    Move local packages to a subdir.

--- common/overlays/default.nix
+++ common/overlays/default.nix
@@ -1,8 +1,8 @@
 {...}: {
   imports = [
+    ./local
     #./dotnet-7.0.nix
     ./dotnet-combine.nix
-    ./local.nix
     #./ms-dotnettools-csharp.nix
     ./prometheus-node-exporter.nix
     #./omnisharp-roslyn.nix
