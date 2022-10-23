edaeb0cfecfa8150598c1877bf60c73749173df0
GGG <gggkiller2@gmail.com>
2022-10-23 20:32:44 -0300

    New mc modpack.

--- hosts/shiro/services/gaming/mnn-server.nix
+++ hosts/shiro/services/gaming/mnn-server.nix
@@ -3,16 +3,20 @@
 in {
   virtualisation.oci-containers.containers.mc-mnn = {
     image = "itzg/minecraft-server:java17-graalvm-ce";
-    ports = ["25565:25565"];
+    ports = [
+      "25565:25565"
+      "25566:25566"
+    ];
     volumes = ["/zfs-main-pool/data/gaming/mc-mnn:/data"];
     environment = {
       VERSION = "1.18.2";
       TYPE = "FABRIC";
-      PACKWIZ_URL = "https://github.com/GGG-KILLER/mc-modpacks/raw/main/mnn/pack.toml";
-      MEMORY = "2G";
+      PACKWIZ_URL = "https://mc.ggg.dev/mnn-2/pack.toml";
+      MEMORY = "4G";
       LOG_TIMESTAMP = "true";
       USE_SIMD_FLAGS = "true";
       USE_AIKAR_FLAGS = "true";
+      SEED = "-2218787502936624024";
 
       OVERRIDE_SERVER_PROPERTIES = "true";
       ALLOW_NETHER = "true";
@@ -37,9 +41,10 @@ in {
     extraOptions = [
       "--dns=192.168.1.1"
       "--ipc=none"
+      "--pull=always"
     ];
   };
 
-  networking.firewall.allowedTCPPorts = [25565];
-  networking.firewall.allowedUDPPorts = [25565];
+  networking.firewall.allowedTCPPorts = [25565 25566];
+  networking.firewall.allowedUDPPorts = [25565 25566];
 }
