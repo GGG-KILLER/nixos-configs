e03e693899a56a3ed9486444d5b9d2da2a9ea2f1
GGG <gggkiller2@gmail.com>
2022-10-26 13:21:03 -0300

    Add a watchdog in the gateway container.

--- hosts/shiro/containers/vpn-gateway.nix
+++ hosts/shiro/containers/vpn-gateway.nix
@@ -63,6 +63,26 @@ in {
         internalIPs = ["192.168.1.0/24"];
         externalInterface = wg-interface;
       };
+
+      # Watchdog
+      systemd.services.wireguard-watchdog = {
+        description = "Service to fix wireguard if it's not working.";
+        after = ["wg-quick-${wg-interface}.service"];
+        partOf = ["wg-quick-${wg-interface}.service"];
+        startAt = "*:0,15,30,45";
+        path = with pkgs; [iproute2 curl systemd];
+        script = ''
+          #! ${pkgs.bash}/bin/bash
+          set -euo pipefail
+
+          if ! curl https://f.ggg.dev/azenv ; then
+            echo "Restarting wireguard..."
+            ip link del ${wg-interface}
+            systemctl restart wg-quick-${wg-interface}.service
+          fi
+        '';
+        serviceConfig.Type = "oneshot";
+      };
     };
   };
 }
