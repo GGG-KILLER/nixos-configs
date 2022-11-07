cef8bcf78f1e42eb14a0731b610614270d8cb817
GGG <gggkiller2@gmail.com>
2022-11-07 09:38:31 -0300

    Disable some unneeded overlays.

--- common/overlays/default.nix
+++ common/overlays/default.nix
@@ -2,11 +2,11 @@
   imports = [
     ./local
     #./dotnet-7.0.nix
-    ./dotnet-combine.nix
+    #./dotnet-combine.nix
     #./ms-dotnettools-csharp.nix
     ./prometheus-node-exporter.nix
     #./omnisharp-roslyn.nix
-    ./rclone.nix
+    #./rclone.nix
     ./virt-v2v.nix
   ];
 }
