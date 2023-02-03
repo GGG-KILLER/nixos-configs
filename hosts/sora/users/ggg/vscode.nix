ac0d69fe1e7297399085384f3956087be9ce49b0
GGG <gggkiller2@gmail.com>
2023-02-03 18:53:35 -0300

    Update VSCode packages.

--- hosts/sora/users/ggg/vscode.nix
+++ hosts/sora/users/ggg/vscode.nix
@@ -2,12 +2,23 @@
   system,
   pkgs,
   inputs,
+  lib,
   ...
-}: let
+}:
+with lib; let
   inherit (builtins) fromJSON readFile map filter;
   readJSON = path: fromJSON (readFile path);
   settings = readJSON ./configs/settings.json;
   inherit (pkgs.vscode-utils) extensionsFromVscodeMarketplace;
+  vscodeExt = {
+    id,
+    version,
+    sha256,
+  }: let
+    parts = splitString "." id;
+    publisher = last (take 1 parts);
+    name = last parts;
+  in {inherit publisher name version sha256;};
 in {
   home-manager.users.ggg = {
     programs.vscode = {
@@ -55,66 +66,56 @@ in {
           wix.vscode-import-cost
         ]
         ++ extensionsFromVscodeMarketplace [
-          {
-            publisher = "cschlosser";
-            name = "doxdocgen";
+          (vscodeExt {
+            id = "cschlosser.doxdocgen";
             version = "1.4.0";
             sha256 = "InEfF1X7AgtsV47h8WWq5DZh6k/wxYhl2r/pLZz9JbU=";
-          }
-          {
-            publisher = "omkov";
-            name = "vscode-ebnf";
-            version = "1.0.4";
-            sha256 = "7qAdCGWTThBAoI/axT+YckEc8h78t7Nq80EwHAudb0s=";
-          }
-          {
-            publisher = "venner";
-            name = "vscode-glua-enhanced";
+          })
+          (vscodeExt {
+            id = "omkov.vscode-ebnf";
+            version = "1.0.5";
+            sha256 = "sha256-TIh8W05pe7/8wo/rFH79uOXOf1mlEXDoNdfuKhHtS1E=";
+          })
+          (vscodeExt {
+            id = "venner.vscode-glua-enhanced";
             version = "2.5.1";
             sha256 = "QLHUFCepddhlsPMrKhxoeI/a6kdHWPCg+BPO2fBu6K4=";
-          }
-          {
-            publisher = "kumar-harsh";
-            name = "graphql-for-vscode";
+          })
+          (vscodeExt {
+            id = "kumar-harsh.graphql-for-vscode";
             version = "1.15.3";
             sha256 = "0Al+69quQXPdFBMsSDWXjITJiux+OQSzQ7i/pgnlm/Q=";
-          }
-          {
-            publisher = "tht13";
-            name = "html-preview-vscode";
+          })
+          (vscodeExt {
+            id = "tht13.html-preview-vscode";
             version = "0.2.5";
             sha256 = "22CeRp/pz0UicMgocfmkd4Tko9Avc96JR9jJ/+KO5Uw=";
-          }
-          {
-            publisher = "christopherstyles";
-            name = "html-entities";
+          })
+          (vscodeExt {
+            id = "christopherstyles.html-entities";
             version = "1.1.2";
             sha256 = "P+IKHH9DDAgvU4Gfsc7c7QYZBE4QPooIteFSzoSoMzw=";
-          }
-          {
-            publisher = "visualstudioexptteam";
-            name = "vscodeintellicode";
-            version = "1.2.25";
-            sha256 = "sha256-MpOZQsenSGBgPpVTlRGvXFgZqfAGwuDzsys8tm7d/FE=";
-          }
-          {
-            publisher = "ms-vscode";
-            name = "powershell";
-            version = "2022.8.5";
-            sha256 = "sha256-QwsVUakeSsdxiYdS1Z2+9jnPEyp0MeXNaaN038wJGCU=";
-          }
-          {
-            publisher = "ms-vscode-remote";
-            name = "remote-containers";
-            version = "0.252.0";
-            sha256 = "sha256-pXd2IjbRwYgUAGVIMLE9mQwR8mG/x0MoMfK8zVh3Mvs=";
-          }
-          {
-            publisher = "L13RARY";
-            name = "l13-diff";
+          })
+          (vscodeExt {
+            id = "visualstudioexptteam.vscodeintellicode";
+            version = "1.2.30";
+            sha256 = "sha256-f2Gn+W0QHN8jD5aCG+P93Y+JDr/vs2ldGL7uQwBK4lE=";
+          })
+          (vscodeExt {
+            id = "ms-vscode.powershell";
+            version = "2023.1.0";
+            sha256 = "sha256-OiVb88BGvzsPTzKU1rGLGSMQSwKV4zI9FthOmutz34U=";
+          })
+          (vscodeExt {
+            id = "ms-vscode-remote.remote-containers";
+            version = "0.276.0";
+            sha256 = "sha256-2LnWUd+ZvTHd16OkaZrYmqHCToBa3c9TSGiJSXLq46Q=";
+          })
+          (vscodeExt {
+            id = "L13RARY.l13-diff";
             version = "1.0.3";
             sha256 = "sha256-dZAtNyMaVmBMGWCVpizK8JDZASzMo4AkIKkjPYWJn1U=";
-          }
+          })
         ];
     };
 
