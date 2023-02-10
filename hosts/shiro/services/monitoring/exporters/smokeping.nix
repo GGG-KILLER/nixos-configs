0843e9a138a518ea4af36d119c001f6ecd777fb1
GGG <gggkiller2@gmail.com>
2023-02-09 23:58:48 -0300

    Add smokeping exporter.
    To detect internet connection outages.

---
+++ hosts/shiro/services/monitoring/exporters/smokeping.nix
@@ -0,0 +1,25 @@
+{config, ...}: {
+  services.prometheus.exporters.smokeping = {
+    enable = true;
+    hosts =
+      [
+        "192.168.1.1"
+      ]
+      # Don't want anyone spamming these with pings so security through
+      # obscurity it is.
+      ++ config.my.secrets.exporters.internet-smokeping-hosts;
+  };
+
+  services.prometheus.scrapeConfigs = [
+    {
+      job_name = "smokeping";
+      static_configs = [
+        {
+          targets = ["127.0.0.1:${toString config.services.prometheus.exporters.smokeping.port}"];
+          labels = {inherit (config.my.constants.prometheus) instance;};
+        }
+      ];
+      inherit (config.my.constants.prometheus) scrape_interval;
+    }
+  ];
+}
