26173a61ef2f044798b323aa1cfd003f507595e6
GGG <gggkiller2@gmail.com>
2023-03-21 21:33:11 -0300

    Remove unneeded overlay.

--- common/overlays/default.nix
+++ common/overlays/default.nix
@@ -1,7 +1,7 @@
 {inputs, ...}: {
   imports = [
     ./local
-    ./ffmpeg-cuda-fix.nix
+    # ./ffmpeg-cuda-fix.nix
     ./prometheus-node-exporter.nix
     ./virt-v2v.nix
     # "${inputs.mats-config}/common/overlays/mpv.nix"
