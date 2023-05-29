da703c4e30645ae96e076c10f5be1036a1124ea0
GGG <gggkiller2@gmail.com>
2023-05-29 10:18:31 -0300

    Enable the aux ports in PC.

--- hosts/sora/audio/51-disable-devices.lua
+++ hosts/sora/audio/51-disable-devices.lua
@@ -1,7 +1,10 @@
+-- wpctl status
+-- wpctl inspect <device id>
+-- Put in device.name here
 local names = {
-    "alsa_card.usb-Sonix_Technology_Co.__Ltd._USB_2.0_Camera_SN0001-02",
-    "alsa_card.pci-0000_06_00.1",
-    "alsa_card.pci-0000_08_00.4",
+    "alsa_card.usb-Sonix_Technology_Co.__Ltd._USB_2.0_Camera_SN0001-02", -- Webcam
+    "alsa_card.pci-0000_06_00.1", -- NVIDIA HDMI Output
+    -- "alsa_card.pci-0000_08_00.4", -- Aux ports
 }
 
 for _, name in ipairs(names)  do
