85c12a4472cfe5fa9160a6028b589966b8a31e69
GGG <gggkiller2@gmail.com>
2023-06-14 10:32:40 -0300

    Update mockoon.

--- common/overlays/local/mockoon.nix
+++ common/overlays/local/mockoon.nix
@@ -4,10 +4,10 @@
 }:
 appimageTools.wrapType2 rec {
   name = "mockoon-${version}";
-  version = "1.23.0";
+  version = "3.0.0";
   src = fetchurl {
     url = "https://github.com/mockoon/mockoon/releases/download/v${version}/mockoon-${version}.AppImage";
-    sha256 = "sha256-1ez+V6bVlyku/zr3puDaPya+P9CQAKTLMF/F23yvpH0=";
+    sha256 = "sha256-YGcD/8h21fUoBEAcBVI5jo0UMCKdVRdC1zxDIrHjU+8=";
   };
   extraPkgs = pkgs: with pkgs; [];
 }
