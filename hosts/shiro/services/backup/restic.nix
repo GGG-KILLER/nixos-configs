44e1d8936df32e30c6552e1f03b567cc11a59b3c
GGG <gggkiller2@gmail.com>
2023-10-26 19:15:01 -0300

    Fix archival exclusion.

--- hosts/shiro/services/backup/restic.nix
+++ hosts/shiro/services/backup/restic.nix
@@ -27,7 +27,7 @@ in {
       extraBackupArgs = [
         "--compression max"
         "--exclude=${baseDir}/etc/random"
-        "--exclude=${baseDir}/etc/Archive"
+        "--exclude=${baseDir}/etc/Archives"
         "--exclude=${baseDir}/h/G"
         "--exclude=${baseDir}/h/Playlists"
         "--exclude=${baseDir}/h/T"
