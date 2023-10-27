38185c517230adae82b15c4d05c793c8b9b05fc2
GGG <gggkiller2@gmail.com>
2023-10-27 19:50:18 -0300

    Update step-ca config.
    - Add restrictions to hostnames certs can be emitted for;
    - Update the allowed challenges;
    - Update the allowed cipher suites.

--- hosts/shiro/services/step-ca.nix
+++ hosts/shiro/services/step-ca.nix
@@ -19,7 +19,10 @@ in {
   # This is only for the nginx config of the downloader.
   modules.services.nginx.virtualHosts."ca.lan" = {
     ssl = true;
-    locations."/".proxyPass = "https://127.0.0.1:${toString step-ca-port}";
+    locations."/" = {
+      proxyPass = "https://127.0.0.1:${toString step-ca-port}";
+      recommendedProxySettings = true;
+    };
     locations."= /root.crt".alias = config.my.secrets.pki.root-crt-path;
     locations."= /intermediate.crt".alias = config.my.secrets.pki.intermediate-crt-path;
   };
@@ -40,36 +43,55 @@ in {
     intermediatePasswordFile = secrets.step-ca-intermediate-key-password.path;
     # See https://smallstep.com/docs/step-ca/configuration#basic-configuration-options
     settings = {
-      dnsNames = ["ca.lan"];
       root = config.my.secrets.pki.root-crt-path;
       crt = config.my.secrets.pki.intermediate-crt-path;
       key = secrets.step-ca-intermediate-key.path;
+      dnsNames = ["ca.lan"];
+      logger.format = "text";
       db = {
         type = "badgerv2";
         dataSource = "/var/lib/step-ca/db";
         badgerFileLoadingMode = "";
       };
       authority = {
-        provisioners = [
-          {
-            type = "ACME";
-            name = "acme";
-            forceCN = true;
-          }
-        ];
         claims = {
           minTLSCertDuration = "5m";
           maxTLSCertDuration = "24h";
           defaultTLSCertDuration = "24h";
         };
+        policy = {
+          x509 = {
+            allow = {
+              dns = ["*.*.shiro.lan" "*.shiro.lan" "*.lan"];
+            };
+            allowWildcardNames = true;
+          };
+        };
+        provisioners = [
+          {
+            type = "ACME";
+            name = "acme";
+            forceCN = true;
+            caaIdentities = ["ca.lan"];
+            challenges = ["http-01"];
+          }
+        ];
         backdate = "1m0s";
       };
       tls = {
         cipherSuites = [
+          "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"
+          "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
           "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
-          "TLS_AES_128_GCM_SHA256"
-          "TLS_AES_256_GCM_SHA384"
-          "TLS_CHACHA20_POLY1305_SHA256"
+          "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA"
+          "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+          "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
+          "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA"
+          "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
+          "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+          "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
+          "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+          "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
         ];
         minVersion = 1.2;
         maxVersion = 1.3;
