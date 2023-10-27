0eb10bee5ec663a51341e259b13a62b8d09fc1d3
GGG <gggkiller2@gmail.com>
2023-10-26 21:29:07 -0300

    Add public file sharing.

--- hosts/shiro/services/nginx.nix
+++ hosts/shiro/services/nginx.nix
@@ -1,5 +1,10 @@
 {...}: {
   modules.services.nginx = {
     enable = true;
+
+    virtualHosts."pub.shiro.lan" = {
+      ssl = true;
+      root = "/zfs-main-pool/data/http-public";
+    };
   };
 }
