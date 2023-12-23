bd356112f4cb3838134e40446e5737905d6df327
GGG <gggkiller2@gmail.com>
2023-12-23 01:25:10 -0300

    Get flatpak-compatible r2modman.

--- common/overlays/default.nix
+++ common/overlays/default.nix
@@ -3,6 +3,7 @@
     ./local
     ./arcmenu.nix # TODO: Remove when NixOS/nixpkgs#270142 lands on unstable
     ./prometheus-node-exporter.nix
+    ./r2modman.nix
     ./torch.nix
     ./virt-v2v.nix
   ];
