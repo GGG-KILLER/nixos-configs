61704d3b1da6ad461e2652bae7764f81dccdf41f
GGG <gggkiller2@gmail.com>
2024-01-11 12:02:27 -0300

    Temporary fix for NixOS/nix#9725.

--- common/overlays/default.nix
+++ common/overlays/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./local
     ./arcmenu.nix # TODO: Remove when NixOS/nixpkgs#270142 lands on unstable
+    ./nix.nix
     ./prometheus-node-exporter.nix
     ./r2modman.nix
     ./torch.nix
