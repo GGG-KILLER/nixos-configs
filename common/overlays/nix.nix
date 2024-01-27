b57e1489e886feb025dcfa5fef71f16852533cfd
GGG <gggkiller2@gmail.com>
2024-01-26 23:57:11 -0300

    Post update flake fixes.

--- common/overlays/nix.nix
+++ common/overlays/nix.nix
@@ -1,13 +1,17 @@
 {lib, ...}: {
   nixpkgs.overlays = [
     (final: prev: {
-#      nix = prev.nix.overrideAttrs (previousAttrs: {
-#        patches = previousAttrs.patches ++ [./nix/getMaxCPU_2_18.patch];
-#      });
-
       nixVersions = lib.mapAttrs (x: y:
         y.overrideAttrs (previousAttrs: {
-          patches = previousAttrs.patches ++ [(if previousAttrs.version == "2.19.2" then ./nix/getMaxCPU_2_19.patch else ./nix/getMaxCPU_2_18.patch)];
+          patches =
+            previousAttrs.patches
+            ++ [
+              (
+                if previousAttrs.version == "2.19.3"
+                then ./nix/getMaxCPU_2_19.patch
+                else ./nix/getMaxCPU_2_18.patch
+              )
+            ];
         }))
       prev.nixVersions;
     })
