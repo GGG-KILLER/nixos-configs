8cb505212e434d30280859112ee61eeb467701cb
GGG <gggkiller2@gmail.com>
2024-01-27 02:57:44 -0300

    Clean up unneeded overlays.

--- common/overlays/arcmenu.nix
+++
@@ -1,7 +0,0 @@
-{lib, ...}: {
-  nixpkgs.overlays = [
-    (self: super: {
-      gnomeExtensions = lib.recursiveUpdate super.gnomeExtensions {arcmenu = super.callPackage ./arcmenu {};};
-    })
-  ];
-}
--- common/overlays/default.nix
+++ common/overlays/default.nix
@@ -1,10 +1,8 @@
 {inputs, ...}: {
   imports = [
     ./local
-    ./arcmenu.nix # TODO: Remove when NixOS/nixpkgs#270142 lands on unstable
     ./nix.nix
     ./prometheus-node-exporter.nix
-    ./r2modman.nix
     ./torch.nix
     ./virt-v2v.nix
   ];
