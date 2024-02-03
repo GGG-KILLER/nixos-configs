3d19815b0d3b8b83324044f4a424070ed303e22a
GGG <gggkiller2@gmail.com>
2024-02-03 16:30:28 -0300

    Update r2modman

--- common/overlays/r2modman.nix
+++ common/overlays/r2modman.nix
@@ -2,13 +2,13 @@
   nixpkgs.overlays = [
     (self: super: {
       r2modman = super.r2modman.overrideDerivation (oldAttrs: rec {
-        version = "3.1.45";
+        version = "3.1.46";
 
         src = super.fetchFromGitHub {
           owner = "PedroVH";
           repo = "r2modmanPlus";
           rev = version;
-          hash = "sha256-1sPj/5ltvgZQQ+6bTA2zSU+fIYdC/4jWzWwIz2vnq5c=";
+          hash = "sha256-m6PREv1h+DHIhUmBA7nZJOiTPoZT5siddKkz9+ZMET4=";
         };
 
         patches = [];
