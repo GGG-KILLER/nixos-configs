648db1af6216150ffae3289463959b252830b22d
GGG <gggkiller2@gmail.com>
2024-02-29 16:51:11 -0300

    Remove permitted insecure electron.

--- common/default.nix
+++ common/default.nix
@@ -25,6 +25,5 @@
 
   nixpkgs.config.permittedInsecurePackages = [
     "openssl-1.1.1w" # TODO: Remove when home-assistant no longer uses deprecated openssl. Blocked on project-chip/connectedhomeip#25688
-    "electron-25.9.0" # r2modman
   ];
 }
