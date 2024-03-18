fad93c4e4ab9ffc61876290d57c8ca748a36a4b1
GGG <gggkiller2@gmail.com>
2024-03-17 23:37:24 -0300

    Add ffmpeg to server path.

--- hosts/shiro/users/ggg/default.nix
+++ hosts/shiro/users/ggg/default.nix
@@ -9,6 +9,13 @@
   ];
 
   home-manager.users.ggg = {
-    home.packages = with pkgs; [tmux self.packages.${system}.yt-dlp aria step-cli self.packages.${system}.kemono-dl];
+    home.packages = with pkgs; [
+      tmux
+      self.packages.${system}.yt-dlp
+      aria
+      step-cli
+      self.packages.${system}.kemono-dl
+      self.packages.${system}.ffmpeg-full
+    ];
   };
 }
