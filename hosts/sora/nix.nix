6b0c411973fc2dca790ece31fff115fdba845327
GGG <gggkiller2@gmail.com>
2024-04-01 01:02:24 -0300

    Remove lower build core limit.

--- hosts/sora/nix.nix
+++ hosts/sora/nix.nix
@@ -1,10 +1,4 @@
 {...}: {
-  # Limit to 10 cores so I don't have a totally unresponsive system.
-  nix.settings = {
-    cores = 11;
-    max-jobs = 11;
-  };
-
   # Automatic garbage collect
   nix.gc = {
     automatic = true;
