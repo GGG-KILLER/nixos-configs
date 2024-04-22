7c8c0fb444e2d4e19aa0e959ecc0bd4083e603d9
GGG <gggkiller2@gmail.com>
2024-04-21 23:20:00 -0300

    Update nix config.

--- hosts/sora/nix.nix
+++ hosts/sora/nix.nix
@@ -1,4 +1,13 @@
 {...}: {
+  nix.settings = {
+    http-connections = 100;
+    max-substitution-jobs = 64;
+    keep-going = true;
+    max-jobs = "auto";
+    use-cgroups = true;
+    warn-dirty = false;
+  };
+
   # Automatic garbage collect
   nix.gc = {
     automatic = true;
