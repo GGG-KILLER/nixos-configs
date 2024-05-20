9f6679423e4b4177f1715f7851237858befb958b
GGG <gggkiller2@gmail.com>
2024-05-20 13:44:20 -0300

    Switch theme from gnome stuff.

--- hosts/sora/kde.nix
+++ hosts/sora/kde.nix
@@ -8,4 +8,10 @@
   # Wayland tweaks
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.xwayland.enable = true;
+
+  # Theming
+  programs.dconf.enable = true;
+  qt.enable = true;
+  qt.platformTheme = "kde";
+  qt.style = "breeze";
 }
--- hosts/sora/users/ggg/default.nix
+++ hosts/sora/users/ggg/default.nix
@@ -42,7 +42,6 @@
 in {
   imports = [
     ./commands
-    ./theme.nix
     ./vscode.nix
   ];
 
