ffd7dc48e3977517b013a8a87e4c1cacd093a67c
GGG <gggkiller2@gmail.com>
2024-06-10 16:56:27 -0300

    Enable KDE Connect.

--- hosts/sora/configuration.nix
+++ hosts/sora/configuration.nix
@@ -104,6 +104,9 @@
   # Enable Waydroid
   virtualisation.waydroid.enable = true;
 
+  # Enable KDE Connect
+  programs.kdeconnect.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
