eecd2ecc2763615660926eec064a6304fa1aab6c
GGG <gggkiller2@gmail.com>
2024-06-20 17:14:02 -0300

    Add Discover to the packages.

--- hosts/sora/kde.nix
+++ hosts/sora/kde.nix
@@ -1,9 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   # Enable KDE.
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  # Add Discover
+  environment.systemPackages = with pkgs.kdePackages; [discover];
 
   # Wayland tweaks
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
