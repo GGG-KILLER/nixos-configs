510dd8a0841c77d2d8cee5c206c708c4a1edcb2b
GGG <gggkiller2@gmail.com>
2024-08-06 00:08:14 -0300

    shiro: fix video drivers not loading

--- hosts/shiro/video.nix
+++ hosts/shiro/video.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   stableDriver = config.boot.kernelPackages.nvidiaPackages.stable;
@@ -10,15 +11,29 @@
     then stableDriver
     else unstableDriver;
 in {
+  boot.kernelModules = ["nvidia-uvm"]; # Needed for VA-API
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.graphics.enable = true;
 
-  environment.sessionVariables.LIBVA_DRIVER_NAME = "nvidia";
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [nvidia-vaapi-driver];
+    enable32Bit = true;
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    VDPAU_DRIVER = "nvidia";
+    NVD_BACKEND = "direct";
+  };
+
   hardware.nvidia = {
     package = nvidiaDriver;
 
     # NOTE: Open kernel module does not work with the Quadro P400
-    modesetting.enable = false;
+    open = false;
+    modesetting.enable = true;
     nvidiaSettings = false;
+    powerManagement.enable = false;
+    forceFullCompositionPipeline = true;
   };
 }
