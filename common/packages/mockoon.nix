cc314f220df9ee775ae84f365533ca24332f44bf
GGG <gggkiller2@gmail.com>
2024-08-17 11:05:44 -0300

    mockoon: 8.0.0 -> 8.4.0

--- common/packages/mockoon.nix
+++ common/packages/mockoon.nix
@@ -1,11 +1,11 @@
 { appimageTools, fetchurl }:
 appimageTools.wrapType2 rec {
   name = "mockoon-${version}";
-  version = "8.0.0";
+  version = "8.4.0";
   src = fetchurl {
-    # https://github.com/mockoon/mockoon/releases/download/v8.0.0/mockoon-8.0.0.x86_64.AppImage
+    # https://github.com/mockoon/mockoon/releases/download/v8.4.0/mockoon-8.4.0.x86_64.AppImage
     url = "https://github.com/mockoon/mockoon/releases/download/v${version}/mockoon-${version}.x86_64.AppImage";
-    sha256 = "sha256-mhUjV8yFXS76kJDj28VeIv4/PlnKos/Ugo9k3RHnRaM=";
+    sha256 = "sha256-AjfoDqTCOJxvcpo08J8rTR8QKjUhs+U7WvR9sK64mVI=";
   };
   meta.mainProgram = "mockoon-${version}";
 }
