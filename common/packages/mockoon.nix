9135e5e7c3b38d793c2831cd02dfe56f57013d99
GGG <gggkiller2@gmail.com>
2024-08-17 11:09:03 -0300

    mockoon: convert to nixpkgs script

--- common/packages/mockoon.nix
+++ common/packages/mockoon.nix
@@ -1,11 +1,38 @@
-{ appimageTools, fetchurl }:
-appimageTools.wrapType2 rec {
-  name = "mockoon-${version}";
+{
+  lib,
+  appimageTools,
+  fetchurl,
+}:
+
+let
+  pname = "mockoon";
   version = "8.4.0";
+
   src = fetchurl {
-    # https://github.com/mockoon/mockoon/releases/download/v8.4.0/mockoon-8.4.0.x86_64.AppImage
     url = "https://github.com/mockoon/mockoon/releases/download/v${version}/mockoon-${version}.x86_64.AppImage";
-    sha256 = "sha256-AjfoDqTCOJxvcpo08J8rTR8QKjUhs+U7WvR9sK64mVI=";
+    hash = "sha256-AjfoDqTCOJxvcpo08J8rTR8QKjUhs+U7WvR9sK64mVI=";
   };
-  meta.mainProgram = "mockoon-${version}";
+
+  appimageContents = appimageTools.extractType2 { inherit pname version src; };
+in
+
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -Dm 444 ${appimageContents}/${pname}.desktop -t $out/share/applications
+    cp -r ${appimageContents}/usr/share/icons $out/share
+
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace 'Exec=AppRun' 'Exec=${pname}'
+  '';
+
+  meta = with lib; {
+    description = "Easiest and quickest way to run mock APIs locally";
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    homepage = "https://mockoon.com";
+    license = licenses.mit;
+    maintainers = with maintainers; [ dit7ya ];
+    mainProgram = "mockoon";
+  };
 }
