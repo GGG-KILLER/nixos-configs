6a1ddaf7134eae6da4e6b9d863bb4feed84b8486
GGG <gggkiller2@gmail.com>
2024-08-24 00:04:43 -0300

    sora: fix OVMF not having support for TPM and secure boot

--- hosts/sora/virtualisation.nix
+++ hosts/sora/virtualisation.nix
@@ -1,11 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 {
   virtualisation.spiceUSBRedirection.enable = true;
 
   # libvirtd
   virtualisation.libvirtd = {
     enable = true;
-    qemu.ovmf.enable = true;
+    qemu.ovmf = {
+      enable = true;
+      packages = [
+        (pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+          msVarsTemplate = true;
+        }).fd
+      ];
+    };
     qemu.swtpm.enable = true;
   };
 }
