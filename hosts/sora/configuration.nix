f80cc50e99314d75f0752f362d7fdd6619f449af
GGG <gggkiller2@gmail.com>
2024-09-24 10:48:12 -0300

    Revert "sora: enable DCHP for USB tethering."
    This reverts commit 9f1f0d55c6d69bfa0ea2fae57c24a97da528c173.

--- hosts/sora/configuration.nix
+++ hosts/sora/configuration.nix
@@ -41,9 +41,8 @@
     enableIPv6 = false; # No ISP support.
 
     useDHCP = false;
-    bridges.br-lan.interfaces = [ "enp5s0" ];
-    interfaces.br-lan.useDHCP = true;
-    interfaces.enp2s0f0u8.useDHCP =  true;
+    bridges."br-lan".interfaces = [ "enp5s0" ];
+    interfaces."br-lan".useDHCP = true;
 
     nameservers = [ "192.168.1.1" ];
   };
