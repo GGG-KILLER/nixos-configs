705af4700f8cc7aa5332a9681c06e7a0254c5355
GGG <gggkiller2@gmail.com>
2024-10-12 21:56:32 -0300

    shiro: migrate to livestreamdvr package.

--- hosts/shiro/services/downloads/live-stream-dvr.nix
+++ hosts/shiro/services/downloads/live-stream-dvr.nix
@@ -7,59 +7,29 @@
   ...
 }:
 {
-  systemd.services.live-stream-dvr =
-    let
-      src = pkgs.fetchzip {
-        url = "https://github.com/MrBrax/LiveStreamDVR/releases/download/LiveStreamDVR-2024-04-15-c2.4.2-s1.7.3-d1.1.2-v2.3.2.1/LiveStreamDVR-2024-04-15-c2.4.2-s1.7.3-d1.1.2-v2.3.2.1.zip";
-        hash = "sha256-c9OgYZXMmK1wSvOx3nnlNQMk9Vm7C+D+gEjNet++S5o=";
-        stripRoot = false;
-      };
-      bin-dir = pkgs.symlinkJoin {
-        name = "live-stream-dvr-bin";
-        paths = with pkgs; [
-          nodejs
-          python39
-          ffmpeg
-          mediainfo
-          self.packages.${system}.twitch-downloader
-          streamlink
-          yt-dlp
-          vcsi
-        ];
-      };
-    in
-    {
-      after = [ "network.target" ];
-      description = "Live Stream DVR";
-      wantedBy = [ "multi-user.target" ];
-      path = [ bin-dir ];
-      environment = {
-        TCD_BIN_DIR = "${lib.getBin bin-dir}/bin";
-        TCD_FFMPEG_PATH = lib.getExe' bin-dir "ffmpeg";
-        TCD_BIN_PATH_PYTHON = lib.getExe' bin-dir "python";
-        TCD_BIN_PATH_PYTHON3 = lib.getExe' bin-dir "python3";
-        TCD_MEDIAINFO_PATH = lib.getExe' bin-dir "mediainfo";
-        TCD_NODE_PATH = lib.getExe' bin-dir "node";
-        TCD_TWITCHDOWNLOADER_PATH = lib.getExe' bin-dir "TwitchDownloaderCLI";
-        TCD_SERVER_PORT = toString config.shiro.ports.live-stream-dvr;
-        TCD_WEBSOCKET_ENABLED = "1";
-        TCD_ENABLE_FILES_API = "1";
-        TCD_EXPOSE_LOGS_TO_PUBLIC = "1";
-        TCD_MIGRATE_OLD_VOD_JSON = "1";
-        TCD_PYTHON_ENABLE_PIPENV = "0";
-      };
-      serviceConfig = {
-        User = "downloader";
-        Group = "data-members";
-        Restart = "always";
-        WorkingDirectory = "${src}/server";
-        ExecStart = ''
-          ${lib.getExe pkgs.nodejs} ./build/server.js \
-            --dataroot /zfs-main-pool/data/services/live-stream-dvr \
-            --port ${toString config.shiro.ports.live-stream-dvr}
-        '';
-      };
+  systemd.services.live-stream-dvr = {
+    after = [ "network.target" ];
+    description = "Live Stream DVR";
+    wantedBy = [ "multi-user.target" ];
+    environment = {
+      TCD_TWITCHDOWNLOADER_PATH = lib.getExe self.packages.${system}.twitch-downloader;
+      TCD_SERVER_PORT = toString config.shiro.ports.live-stream-dvr;
+      TCD_WEBSOCKET_ENABLED = "1";
+      TCD_ENABLE_FILES_API = "1";
+      TCD_EXPOSE_LOGS_TO_PUBLIC = "1";
+      TCD_MIGRATE_OLD_VOD_JSON = "1";
     };
+    serviceConfig = {
+      User = "downloader";
+      Group = "data-members";
+      Restart = "always";
+      ExecStart = ''
+        ${lib.getExe self.packages.${system}.livestreamdvr} \
+          --dataroot /zfs-main-pool/data/services/live-stream-dvr \
+          --port ${toString config.shiro.ports.live-stream-dvr}
+      '';
+    };
+  };
 
   modules.services.nginx.virtualHosts."ttv.ggg.dev" = {
     ssl = true;
