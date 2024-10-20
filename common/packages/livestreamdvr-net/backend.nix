06433b99e326f6aa465b496df7d84d290b6b201d
GGG <gggkiller2@gmail.com>
2024-10-20 03:22:29 -0300

    livestreamdvr-net: 1-unstable-2024-10-19 -> 0-unstable-2024-10-20

--- common/packages/livestreamdvr-net/backend.nix
+++ common/packages/livestreamdvr-net/backend.nix
@@ -7,13 +7,13 @@
 
 buildDotnetModule rec {
   pname = "livestreamdvr-net-backend";
-  version = "1-unstable-2024-10-19";
+  version = "0-unstable-2024-10-20";
 
   src = fetchFromGitHub {
     owner = "GGG-KILLER";
     repo = "LiveStreamDVR.NET";
-    rev = "ba4a8e29c971e76747de2591236b5e15d01151cf";
-    hash = "sha256-cnZG7RA7cJrOyFQQSm5qbHa5l6Oe8tVemfJlgt7zYVQ=";
+    rev = "a2052c123b576e98451f5b11912ebbdc9386fdde";
+    hash = "sha256-cg/SdSwF9zuX3Hx4DR0M1dnT+r6EcwacrV/oH19Q/lc=";
   };
 
   projectFile = "backend/src/LiveStreamDVR.Api.csproj";
