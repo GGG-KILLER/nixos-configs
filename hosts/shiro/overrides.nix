5729943d34b2a9d1f9c0f018b335835019e7edd6
GGG <gggkiller2@gmail.com>
2024-10-31 14:29:46 -0300

    shiro: remove step-ca src override.

--- hosts/shiro/overrides.nix
+++ hosts/shiro/overrides.nix
@@ -1,15 +1,5 @@
 { ... }:
 {
   nixpkgs.overlays = [
-    (self: super: {
-      step-ca = super.step-ca.overrideDerivation (oldAttrs: {
-        src = super.fetchFromGitHub {
-          owner = "smallstep";
-          repo = "certificates";
-          rev = "refs/tags/v${oldAttrs.version}";
-          hash = "sha256-byVWNab6Q3yryluhMomzLkRNfXQ/68pAq+YGFjbvX1o=";
-        };
-      });
-    })
   ];
 }
