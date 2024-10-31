3f4bd7ef23e79fa04a0233b0d54af47824ab0c90
GGG <gggkiller2@gmail.com>
2024-10-31 17:20:42 -0300

    shiro: monitor systemd-nspawn containers as well.

--- hosts/shiro/services/monitoring/cadvisor.nix
+++ hosts/shiro/services/monitoring/cadvisor.nix
@@ -1,4 +1,8 @@
-{config, ...}:{
+{ config, ... }:
+{
   services.cadvisor.enable = true;
   services.cadvisor.port = config.shiro.ports.cadvisor;
+  services.cadvisor.extraOptions = [
+    "--raw_cgroup_prefix_whitelist=/machine.slice/"
+  ];
 }
