d1da1b51f23541cc29a433d7b8f0ea634c769c4e
GGG <gggkiller2@gmail.com>
2024-11-05 21:01:48 -0300

    Accept paths relative to the root directory as well.

--- secrets/edit.sh
+++ secrets/edit.sh
@@ -3,4 +3,14 @@
 # shellcheck shell=bash
 ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 pushd "$ROOT_DIR" || exit
-EDITOR="code --wait" exec agenix -e "$@"
+FILE_PATH="$1"
+if [ -z "$FILE_PATH" ]; then
+    echo "Usage: $0 [path]"
+    exit 1
+fi
+
+if [ ! -f "$FILE_PATH" ] && [ -f "../$FILE_PATH" ]; then
+    FILE_PATH=$(realpath -s --relative-to="$ROOT_DIR" "../$FILE_PATH")
+fi
+
+EDITOR="code --wait" exec agenix -e "$FILE_PATH"
