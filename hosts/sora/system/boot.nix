de5b8b714c0c247b21514aef1d1040476e7cc50e
GGG <gggkiller2@gmail.com>
2025-01-07 13:49:11 -0300

    sora: add memtest86.

--- hosts/sora/system/boot.nix
+++ hosts/sora/system/boot.nix
@@ -13,5 +13,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.editor = false;
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
