64199b1ce3eb3fa1be2d4f95ae156a62ada281ce
GGG <gggkiller2@gmail.com>
2025-01-28 09:40:33 -0300

    m3u8-dl: update.

--- packages/m3u8-dl.nix
+++ packages/m3u8-dl.nix
@@ -1,10 +1,13 @@
-{ fetchFromGitHub, callPackage }:
+{
+  fetchFromGitHub,
+  callPackage,
+}:
 let
   src = fetchFromGitHub {
     owner = "GGG-KILLER";
     repo = "m3u8-dl";
-    rev = "59b3464a810de311167278a3b4be371ad7741ef7";
-    hash = "sha256-G5/i28wlxWG0jskVbvz9AWm6g7Ml/hOXW41RcM0tS8E=";
+    rev = "c073e522fae317425125276c69fcdb790cd0c3da";
+    hash = "sha256-YH2mHURNVEoayv6hL66Q1SB8wjE/lSbCr0GBxYHRLro=";
   };
 in
 callPackage src { }
