dc5742469b15805d10b6f298078b0cc89cbe3d70
GGG <gggkiller2@gmail.com>
2025-02-16 01:28:35 -0300

    sora: add libraries to nix-ld.

--- hosts/sora/system/default.nix
+++ hosts/sora/system/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./desktop
@@ -12,6 +12,18 @@
 
   # Giving up on 100% pure nix, I want .NET AOT
   programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    glibc # libdl
+    gtk3 # libglib-2.0.so.0 libgobject-2.0.so.0 libgtk-3.so.0 libgdk-3.so.0
+    libGL # libGL.so.1
+    xorg.libICE # libICE.so.6
+    xorg.libSM # libSM.so.6
+    xorg.libX11 # libX11 libX11.so.6
+    xorg.libXcursor # libXcursor.so.1
+    xorg.libXi # libXi.so.6
+    xorg.libXrandr # libXrandr.so.2
+    fontconfig # libfontconfig.so.1
+  ];
 
   # Enable Firejail
   programs.firejail.enable = true;
