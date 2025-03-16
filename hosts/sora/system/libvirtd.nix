b6fceb2da2c782a592deb1eaeb243e473909e2d2
GGG <gggkiller2@gmail.com>
2025-03-16 03:19:44 -0300

    sora: run libvirtd as root.

--- hosts/sora/system/libvirtd.nix
+++ hosts/sora/system/libvirtd.nix
@@ -6,7 +6,7 @@
   virtualisation.libvirtd.enable = true;
 
   virtualisation.libvirtd.qemu.package = pkgs.qemu_kvm;
-  virtualisation.libvirtd.qemu.runAsRoot = false;
+  virtualisation.libvirtd.qemu.runAsRoot = true;
 
   virtualisation.libvirtd.qemu.ovmf.enable = true;
   virtualisation.libvirtd.qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
