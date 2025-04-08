e33e1044dd64e59d86fca32e5482dc4738029ed5
GGG <gggkiller2@gmail.com>
2025-04-08 17:14:32 -0300

    vscode-extensions.ms-dotnettools.csdevkit: 1.16.6 -> 1.19.4

--- packages/ms-dotnettools.csdevkit/package.nix
+++ packages/ms-dotnettools.csdevkit/package.nix
@@ -14,19 +14,19 @@ let
     {
       x86_64-linux = {
         arch = "linux-x64";
-        hash = "sha256-zaadrou0TSEcmbxbtNspojI5q/N4oN88AFKTCmJq9tk=";
+        hash = "sha256-F4J9BDaFxEfym4lE8qmbVlGRVchqdP4MGPragkoTSEo=";
       };
       aarch64-linux = {
         arch = "linux-arm64";
-        hash = "sha256-mhPR21ivcFrF1Rg4QOpWIv8G1LNmpiPhhyX7Npbmagc=";
+        hash = "sha256-+13WfWRBbFiImhfuv0XvPbiJ4preSHX2TT+wNA3QShw=";
       };
       x86_64-darwin = {
         arch = "darwin-x64";
-        hash = "sha256-0QC3mn01/HKgZ/WUVn1RTHvDWRtWRJfZzYIdzJIACV4=";
+        hash = "sha256-unVb8SWVsAz2Ih/tlDvdz3Xvf4gHWOf+9lIfhG8pKgo=";
       };
       aarch64-darwin = {
         arch = "darwin-arm64";
-        hash = "sha256-v7jCiiZFbBM2PW4Hmrjzjv5/6Uts3TocE2y8czMs+so=";
+        hash = "sha256-MklLr4oEnlpORNn5PqdwuUJJcSbxz9dIYnOH16IQ9J0=";
       };
     }
     .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}")
@@ -36,7 +36,7 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "csdevkit";
     publisher = "ms-dotnettools";
-    version = "1.17.64";
+    version = "1.19.4";
     inherit (extInfo) hash arch;
   };
   sourceRoot = "extension"; # This has more than one folder.
@@ -59,7 +59,7 @@ vscode-utils.buildVscodeMarketplaceExtension {
 
     substituteInPlace dist/extension.js \
       --replace-fail 'e.extensionPath,"cache"' 'require("os").tmpdir(),"'"$ext_unique_id"'"' \
-      --replace-fail 't.setExecuteBit=async function(e){if("win32"!==process.platform){const t=i.join(e[a.SERVICEHUB_CONTROLLER_COMPONENT_NAME],"Microsoft.ServiceHub.Controller"),r=i.join(e[a.SERVICEHUB_HOST_COMPONENT_NAME],(0,a.getServiceHubHostEntrypointName)()),n=[(0,a.getServerPath)(e),t,r,(0,c.getReliabilityMonitorPath)(e)];await Promise.all(n.map((e=>(0,o.chmod)(e,"0755"))))}};' 't.setExecuteBit=async function(e){};'
+      --replace-fail 't.setExecuteBit=async function(e){if("win32"!==process.platform){const t=i.join(e[a.SERVICEHUB_CONTROLLER_COMPONENT_NAME],"Microsoft.VisualStudio.Code.ServiceController"),r=i.join(e[a.SERVICEHUB_HOST_COMPONENT_NAME],(0,a.getServiceHubHostEntrypointName)()),n=[(0,a.getServerPath)(e),t,r,(0,c.getReliabilityMonitorPath)(e)];await Promise.all(n.map((e=>(0,o.chmod)(e,"0755"))))}}' 't.setExecuteBit=async function(e){}'
   '';
 
   preFixup = ''
