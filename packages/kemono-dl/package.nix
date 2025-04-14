a62eb1756d0c17c6f8e388cba3b2ececc38549db
GGG <gggkiller2@gmail.com>
2025-04-14 03:25:06 -0300

    kemono-dl: update

--- packages/kemono-dl/package.nix
+++ packages/kemono-dl/package.nix
@@ -1,11 +1,11 @@
 { buildDotnetModule, dotnetCorePackages }:
 buildDotnetModule rec {
   pname = "kemono-dl";
-  version = "3-unstable-2025-02-09";
+  version = "0-unstable-2025-04-14";
 
   src = builtins.fetchGit {
     url = "git@github.com:GGG-KILLER/kemono-dl.git";
-    rev = "4603390cab70c252be05b225f7ae78ff48a96fb3";
+    rev = "c70747a6a0ff0c1216a8095be73778ce2966eb0d";
   };
 
   projectFile = "KemonoDl.Console/KemonoDl.Console.csproj";
