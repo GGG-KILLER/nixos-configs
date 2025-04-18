3553a0247d667bd3d83937e52d63625ba592c1c5
GGG <gggkiller2@gmail.com>
2025-04-18 05:51:14 -0300

    media: update build script.

--- build-live-cds.sh
+++ build-live-cds.sh
@@ -1,29 +1,31 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p nix
+#! nix-shell -i bash -p nix nix-output-monitor
 # shellcheck shell=bash
 set -euo pipefail
 
 if [ -d isos/ ]; then
-    rm -f isos/*.iso || true;
+    rm -f isos/*.iso || true
 else
-    mkdir isos;
+    mkdir isos
 fi
 
 function move_iso() {
     ISOS=("$1"/iso/*.iso)
     ISO_PATH="${ISOS[0]}"
     ISO_NAME="$(basename "$ISO_PATH")"
-    cp "$ISO_PATH" "$ISO_NAME";
+    cp "$ISO_PATH" "$ISO_NAME"
     chown "$(id -u):$(id -g)" "$ISO_NAME"
     chmod a=r "$ISO_NAME"
 }
 
-pushd isos;
+pushd isos
+(
     echo Creating GNOME ISO...
-    nix build ..#nixosConfigurations.live-cd-gnome.config.system.build.isoImage -o gnome --quiet;
-    move_iso gnome;
+    nom build ..#nixosConfigurations.live-cd-gnome.config.system.build.isoImage -o gnome --quiet
+    move_iso gnome
 
     echo Creating Minimal ISO...
-    nix build ..#nixosConfigurations.live-cd-minimal.config.system.build.isoImage -o minimal --quiet;
-    move_iso minimal;
-popd;
+    nom build ..#nixosConfigurations.live-cd-minimal.config.system.build.isoImage -o minimal --quiet
+    move_iso minimal
+)
+popd
