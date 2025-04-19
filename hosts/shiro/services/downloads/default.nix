c307065a53a4b2d0e7b92dfa94f483cb8cfb843b
GGG <gggkiller2@gmail.com>
2025-04-19 10:19:13 -0300

    shiro: re-enable a few things.

--- hosts/shiro/services/downloads/default.nix
+++ hosts/shiro/services/downloads/default.nix
@@ -2,8 +2,8 @@
 {
   imports = [
     # ./downloader.nix # TODO: Re-enable when job again.
-    # ./live-stream-dvr.nix # TODO: Re-enable when job again.
+    ./live-stream-dvr.nix
     # ./megasync.nix # TODO: Re-enable when Patreon subs restart
-    ./sonarr.nix # TODO: Re-enable when job again.
+    ./sonarr.nix
   ];
 }
--- hosts/shiro/services/monitoring/default.nix
+++ hosts/shiro/services/monitoring/default.nix
@@ -1,10 +1,10 @@
 { ... }:
 {
   imports = [
-    # ./exporters # TODO: Re-enable when job again.
-    # ./netprobe # TODO: Re-enable when job again.
-    # ./grafana.nix # TODO: Re-enable when job again.
-    # ./prometheus.nix # TODO: Re-enable when job again.
+    ./exporters
+    ./netprobe
+    ./grafana.nix
+    ./prometheus.nix
     ./smartd.nix
   ];
 }
