49175d605a37194ca4dc6aece27007acff496b4c
GGG <gggkiller2@gmail.com>
2025-04-19 10:19:00 -0300

    sora/ggg: re-enable jellyfin mpv.

--- hosts/sora/users/ggg/default.nix
+++ hosts/sora/users/ggg/default.nix
@@ -114,17 +114,16 @@ in
       # opensnitch-ui.enable = true;
     };
 
-    # TODO: re-enable when server is up
-    # systemd.user.services.jellyfin-mpv-shim = {
-    #   Unit = {
-    #     Description = "Jellyfin MPV Shim";
-    #     After = [ "graphical-session-pre.target" ];
-    #     PartOf = [ "graphical-session.target" ];
-    #   };
+    systemd.user.services.jellyfin-mpv-shim = {
+      Unit = {
+        Description = "Jellyfin MPV Shim";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
 
-    #   Service.ExecStart = getExe pkgs.jellyfin-mpv-shim;
+      Service.ExecStart = getExe pkgs.jellyfin-mpv-shim;
 
-    #   Install.WantedBy = [ "graphical-session.target" ];
-    # };
+      Install.WantedBy = [ "graphical-session.target" ];
+    };
   };
 }
