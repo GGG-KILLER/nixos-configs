b2612e5a97f2f973986707e3696199b2a3cbda24
GGG <gggkiller2@gmail.com>
2025-04-21 20:54:48 -0300

    ggg: fix zsh lacking home-manager configs.

--- common/users/ggg/programs.nix
+++ common/users/ggg/programs.nix
@@ -10,6 +10,39 @@
           "-g"
         ];
       };
+      zsh = {
+        autosuggestion.enable = true;
+        autosuggestion.strategy = [ "history" "completion" ];
+        enable = true;
+        enableCompletion = true;
+        enableVteIntegration = true;
+        history.append = true;
+        history.extended = true;
+        history.findNoDups = true;
+        history.ignorePatterns = [ "rm *" "pkill *" ];
+        history.ignoreSpace = true;
+        history.save = 1000000;
+        history.share = true;
+        history.size = 1000000;
+        oh-my-zsh.enable = true;
+        oh-my-zsh.plugins = [ "encode64" "sudo" "timer" "tmux" ];
+        oh-my-zsh.theme = "dpoggi";
+        oh-my-zsh.extraConfig = ''
+          ZSH_TMUX_AUTOSTART_ONCE=true
+          ZSH_TMUX_AUTOCONNECT=true
+          ZSH_TMUX_DETACHED=true
+          ZSH_TMUX_FIXTERM=true
+          ZSH_TMUX_UNICODE=true
+          if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" && -z "$INSIDE_EMACS" && "$TERM_PROGRAM" != "vscode" && "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]] && \
+            [[ -n "$SSH_TTY" ]]; then
+            ZSH_TMUX_DEFAULT_SESSION_NAME="''${USER}@$(awk '{print $1}'<<<"$SSH_CLIENT" | sed 's/\./_/g')"
+            ZSH_TMUX_AUTOSTART=true
+            ZSH_TMUX_AUTOQUIT=true
+          fi
+        '';
+        syntaxHighlighting.enable = true;
+        syntaxHighlighting.highlighters = [ "main" "brackets" ];
+      };
     };
 
     home.file = {
