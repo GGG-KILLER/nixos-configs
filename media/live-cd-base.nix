6062ec98b0c94807a30080b70d71402fa2c61075
GGG <gggkiller2@gmail.com>
2025-04-23 05:10:17 -0300

    media: refactor base.

--- media/live-cd-base.nix
+++ media/live-cd-base.nix
@@ -1,89 +1,65 @@
 {
   self,
-  lib,
   config,
   pkgs,
   ...
 }:
-let
-  inherit (lib) mkOption types;
-in
 {
-  imports = [
-    ../common/groups
-    ../common/users
-    ../common/overlays
-    ../common/secrets/pki.nix
-    ../common/console.nix
-    ../common/i18n.nix
-    ../common/nix.nix
-    ../common/pki.nix
-    ../common/time.nix
+  # ISO Options
+  isoImage.makeEfiBootable = true;
+  isoImage.makeUsbBootable = true;
+  isoImage.storeContents = [
+    self.nixosConfigurations.sora.config.system.build.toplevel
+    self.nixosConfigurations.shiro.config.system.build.toplevel
   ];
 
-  options = {
-    home-manager = mkOption { type = types.any; };
-    modules.home = mkOption { type = types.any; };
+  # Enable ZFS
+  boot.zfs.package = pkgs.zfs_unstable;
+
+  # NVIDIA drivers are unfree.
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Don't need firewall while installing, probably
+  networking.firewall.enable = false;
+
+  # Allow to login with the ggg user SSH key
+  users.users.nixos.openssh.authorizedKeys.keys = config.users.users.ggg.openssh.authorizedKeys.keys;
+
+  # Graphics Settings
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics.enable = true;
+
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "nvidia";
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # NOTE: Open kernel module does not work with the Quadro P400
+    open = false;
+    modesetting.enable = false;
+    nvidiaSettings = false;
   };
 
-  config = {
-    # Use latest kernel if possible.
-    # boot.kernelPackages = pkgs.linuxPackages_latest;
+  environment.systemPackages = with pkgs; [
+    croc
+    wget
+    btop
+    file
+    iotop-c
+    unzip
+    zip
 
-    # ISO Options
-    isoImage.makeEfiBootable = true;
-    isoImage.makeUsbBootable = true;
-    isoImage.storeContents = [
-      self.nixosConfigurations.sora.config.system.build.toplevel
-      self.nixosConfigurations.shiro.config.system.build.toplevel
-    ];
+    tmux
 
-    # Enable ZFS
-    boot.zfs.package = pkgs.zfs_unstable;
+    btrfs-progs
+  ];
 
-    # NVIDIA drivers are unfree.
-    nixpkgs.config.allowUnfree = true;
+  system.activationScripts.linkConfig = ''
+    # Link source to /var/nixos-config
+    ln -sfn ${toString self} /var/nixos-config
+  '';
 
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
-
-    # Don't need firewall while installing, probably
-    networking.firewall.enable = false;
-
-    # Allow to login with the ggg user SSH key
-    users.users.nixos.openssh.authorizedKeys.keys = config.users.users.ggg.openssh.authorizedKeys.keys;
-
-    # Graphics Settings
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.graphics.enable = true;
-
-    environment.sessionVariables.LIBVA_DRIVER_NAME = "nvidia";
-    hardware.nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-      # NOTE: Open kernel module does not work with the Quadro P400
-      open = false;
-      modesetting.enable = false;
-      nvidiaSettings = false;
-    };
-
-    environment.systemPackages = with pkgs; [
-      croc
-      wget
-      btop
-      file
-      iotop-c
-      unzip
-      zip
-
-      tmux
-
-      btrfs-progs
-    ];
-
-    system.activationScripts.linkConfig = ''
-      # Link source to /var/nixos-config
-      ln -sfn ${toString self} /var/nixos-config
-    '';
-  };
+  nixpkgs.hostPlatform = "x86_64-linux";
 }
