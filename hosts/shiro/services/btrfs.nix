4513b85003cc67546e3d99eb5aaa4e6c15f832c6
GGG <gggkiller2@gmail.com>
2025-04-25 05:46:45 -0300

    sora: enable BEES service to deduplicate blocks.

--- hosts/shiro/services/btrfs.nix
+++ hosts/shiro/services/btrfs.nix
@@ -1,8 +1,22 @@
 { ... }:
 {
-  services.btrfs.autoScrub = {
-    enable = true;
-    interval = "weekly";
-    fileSystems = [ "/" ];
+  services.btrfs.autoScrub.enable = true;
+  services.btrfs.autoScrub.fileSystems = [ "/" ];
+  services.btrfs.autoScrub.interval = "weekly";
+
+  services.beesd.filesystems.root = {
+    spec = "/";
+    hashTableSizeMB = 4 * 1024;
+    extraOptions = [
+      "--timestamps"
+      "--thread-min"
+      "2"
+      "--loadavg-target"
+      "8.0"
+      # TODO: Enable when v0.11 releases
+      # "--throttle-factor"
+      # "1"
+    ];
+    verbosity = "warning";
   };
 }
