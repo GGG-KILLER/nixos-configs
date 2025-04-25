d9ee1de81b65d61891ea54e9149f93d23298d305
GGG <gggkiller2@gmail.com>
2025-04-25 02:31:09 -0300

    common: fix home-manager configs being set when it is not enabled.

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -1,21 +1,24 @@
-{ lib, ... }:
+{ lib, options, ... }:
 {
-  home-manager.sharedModules = [
-    (
-      { inputs, config, ... }:
-      {
-        home.stateVersion = "21.03";
+  # only set configs if home-manager option exists
+  config = lib.optionalAttrs (options ? home-manager) {
+    home-manager.sharedModules = [
+      (
+        { inputs, config, ... }:
+        {
+          home.stateVersion = "21.03";
 
-        home.activation.eliminateChannelsRoot = config.lib.dag.entryAfter [ "writeBoundary" ] ''
-          # Get rid of channels
-          rm -f ${config.home.homeDirectory}/.nix-channels
-          rm -rf ${config.home.homeDirectory}/.nix-defexpr
-          ln -sf ${inputs.nixpkgs} ${config.home.homeDirectory}/.nix-defexpr
+          home.activation.eliminateChannelsRoot = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+            # Get rid of channels
+            rm -f ${config.home.homeDirectory}/.nix-channels
+            rm -rf ${config.home.homeDirectory}/.nix-defexpr
+            ln -sf ${inputs.nixpkgs} ${config.home.homeDirectory}/.nix-defexpr
 
-          # Clean up older config versions
-          ${lib.getExe config.nix.package} profile wipe-history --profile ${config.home.homeDirectory}/.local/state/nix/profiles/home-manager
-        '';
-      }
-    )
-  ];
+            # Clean up older config versions
+            ${lib.getExe config.nix.package} profile wipe-history --profile ${config.home.homeDirectory}/.local/state/nix/profiles/home-manager
+          '';
+        }
+      )
+    ];
+  };
 }
--- common/users/ggg/programs.nix
+++ common/users/ggg/programs.nix
@@ -1,52 +1,75 @@
-{ inputs, system, ... }:
 {
-  home-manager.users.ggg = {
-    programs = {
-      dircolors.enable = true;
-      eza = {
-        enable = true;
-        extraOptions = [
-          "-a"
-          "-g"
-        ];
+  lib,
+  options,
+  inputs,
+  system,
+  ...
+}:
+{
+  # only set configs if home-manager option exists
+  config = lib.optionalAttrs (options ? home-manager) {
+    home-manager.users.ggg = {
+      programs = {
+        dircolors.enable = true;
+        eza = {
+          enable = true;
+          extraOptions = [
+            "-a"
+            "-g"
+          ];
+        };
+        zsh = {
+          autosuggestion.enable = true;
+          autosuggestion.strategy = [
+            "history"
+            "completion"
+          ];
+          enable = true;
+          enableCompletion = true;
+          enableVteIntegration = true;
+          history.append = true;
+          history.extended = true;
+          history.findNoDups = true;
+          history.ignorePatterns = [
+            "rm *"
+            "pkill *"
+          ];
+          history.ignoreSpace = true;
+          history.save = 1000000;
+          history.share = true;
+          history.size = 1000000;
+          oh-my-zsh.enable = true;
+          oh-my-zsh.plugins = [
+            "encode64"
+            "sudo"
+            "timer"
+            "tmux"
+          ];
+          oh-my-zsh.theme = "dpoggi";
+          oh-my-zsh.extraConfig = ''
+            ZSH_TMUX_AUTOSTART_ONCE=true
+            ZSH_TMUX_AUTOCONNECT=true
+            ZSH_TMUX_DETACHED=true
+            ZSH_TMUX_FIXTERM=true
+            ZSH_TMUX_UNICODE=true
+            if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" && -z "$INSIDE_EMACS" && "$TERM_PROGRAM" != "vscode" && "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]] && \
+              [[ -n "$SSH_TTY" ]]; then
+              ZSH_TMUX_DEFAULT_SESSION_NAME="''${USER}@$(awk '{print $1}'<<<"$SSH_CLIENT" | sed 's/\./_/g')"
+              ZSH_TMUX_AUTOSTART=true
+              ZSH_TMUX_AUTOQUIT=true
+            fi
+          '';
+          syntaxHighlighting.enable = true;
+          syntaxHighlighting.highlighters = [
+            "main"
+            "brackets"
+          ];
+        };
       };
-      zsh = {
-        autosuggestion.enable = true;
-        autosuggestion.strategy = [ "history" "completion" ];
-        enable = true;
-        enableCompletion = true;
-        enableVteIntegration = true;
-        history.append = true;
-        history.extended = true;
-        history.findNoDups = true;
-        history.ignorePatterns = [ "rm *" "pkill *" ];
-        history.ignoreSpace = true;
-        history.save = 1000000;
-        history.share = true;
-        history.size = 1000000;
-        oh-my-zsh.enable = true;
-        oh-my-zsh.plugins = [ "encode64" "sudo" "timer" "tmux" ];
-        oh-my-zsh.theme = "dpoggi";
-        oh-my-zsh.extraConfig = ''
-          ZSH_TMUX_AUTOSTART_ONCE=true
-          ZSH_TMUX_AUTOCONNECT=true
-          ZSH_TMUX_DETACHED=true
-          ZSH_TMUX_FIXTERM=true
-          ZSH_TMUX_UNICODE=true
-          if [[ -z "$TMUX" && -z "$EMACS" && -z "$VIM" && -z "$INSIDE_EMACS" && "$TERM_PROGRAM" != "vscode" && "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]] && \
-            [[ -n "$SSH_TTY" ]]; then
-            ZSH_TMUX_DEFAULT_SESSION_NAME="''${USER}@$(awk '{print $1}'<<<"$SSH_CLIENT" | sed 's/\./_/g')"
-            ZSH_TMUX_AUTOSTART=true
-            ZSH_TMUX_AUTOQUIT=true
-          fi
-        '';
-        syntaxHighlighting.enable = true;
-        syntaxHighlighting.highlighters = [ "main" "brackets" ];
-      };
-    };
 
-    home.file = {
-      ".cache/nix-index/files".source = inputs.nix-index-database.packages.${system}.nix-index-database;
+      home.file = {
+        ".cache/nix-index/files".source = inputs.nix-index-database.packages.${system}.nix-index-database;
+      };
     };
   };
 }
