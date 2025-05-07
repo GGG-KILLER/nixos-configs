43e54cc58fe9abcaa5085f979a0d552ef950ceb2
GGG <gggkiller2@gmail.com>
2025-05-07 16:11:41 -0300

    sora/audio: disable jack.

--- hosts/sora/system/desktop/audio/default.nix
+++ hosts/sora/system/desktop/audio/default.nix
@@ -7,8 +7,8 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
 
     wireplumber = {
       enable = true;
