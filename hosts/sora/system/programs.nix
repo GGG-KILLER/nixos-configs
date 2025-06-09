ee86cc8fb09406803397b7c11901b3958b450bab
GGG <gggkiller2@gmail.com>
2025-06-09 12:04:07 -0300

    sora: copy dotnet files instead of symlink.

--- hosts/sora/system/programs.nix
+++ hosts/sora/system/programs.nix
@@ -2,19 +2,37 @@
   system,
   self,
   inputs,
+  lib,
   pkgs,
   config,
   ...
 }:
 let
-  dotnet-sdk = (
-    with pkgs.dotnetCorePackages;
-    combinePackages [
-      sdk_10_0
-      sdk_9_0
-      sdk_8_0
-    ]
-  );
+  dotnet-sdk =
+    (
+      with pkgs.dotnetCorePackages;
+      combinePackages [
+        sdk_10_0
+        sdk_9_0
+        sdk_8_0
+      ]
+    ).overrideAttrs
+      (prev: {
+        postInstall =
+          prev.postInstall or ""
+          + ''
+            # Un-link things to avoid problems
+            find "$out" -type l -exec sh -c 'PREV=$(realpath -- "$1") && echo "  $PREV -> $1" && rm -- "$1" && cp --archive --dereference --recursive -- "$PREV" "$1"' resolver {} \;
+
+            # Fix dotnet not finding host/fxr
+            rm "$out"/bin/dotnet
+            ln -s "$out"/share/dotnet/dotnet "$out"/bin/dotnet
+          ''
+          + lib.optionalString (prev.src ? man) ''
+            # Un-link things to avoid problems
+            find "$man" -type l -exec sh -c 'PREV=$(realpath -- "$1") && echo "  $PREV -> $1" && rm -- "$1" && cp --archive --dereference --recursive -- "$PREV" "$1"' resolver {} \;
+          '';
+      });
   dotnetRoot = "${dotnet-sdk}/share/dotnet";
 
   agenix = inputs.agenix.packages.${system}.default;
