29a895da52e336debb6290f7338f3a2b5dc17d01
GGG <gggkiller2@gmail.com>
2025-06-23 22:49:59 -0300

    sora/overrides: vscode-extensions.ms-dotnettools.csdevkit temporary fix.

--- hosts/sora/overrides.nix
+++ hosts/sora/overrides.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   nixpkgs.overlays = [
     (self: super: {
@@ -12,6 +12,20 @@
           mpris
           inhibit-gnome
         ];
+      };
+    })
+    # TODO: Remove when NixOS/nixpkgs#419069 hits unstable. https://nixpkgs-tracker.ocfox.me/?pr=419069
+    (final: prev: {
+      vscode-extensions = lib.recursiveUpdate prev.vscode-extensions {
+        ms-dotnettools.csdevkit = prev.vscode-extensions.ms-dotnettools.csdevkit.overrideAttrs (prev: {
+          preFixup =
+            prev.preFixup
+            + ''
+              # Fix libxml2 breakage. See https://github.com/NixOS/nixpkgs/pull/396195#issuecomment-2881757108
+              mkdir -p "$out/lib"
+              ln -s "${lib.getLib final.libxml2}/lib/libxml2.so" "$out/lib/libxml2.so.2"
+            '';
+        });
       };
     })
   ];
