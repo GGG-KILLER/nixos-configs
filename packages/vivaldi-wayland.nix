6ddc2607c6182883c79a7ddc3d5b462376a4ad39
GGG <gggkiller2@gmail.com>
2025-07-14 03:02:35 -0300

    vivaldi: remove unneeded.

--- packages/vivaldi-wayland.nix
+++ packages/vivaldi-wayland.nix
@@ -1,13 +1,8 @@
-# Source #1: https://github.com/NixOS/nixpkgs/pull/292148#issuecomment-2343586641
-# Source #2: https://github.com/matklad/config/blob/8062c8b8a15eabc7e623d2dab9e98cc8b26bdc48/hosts/packages.nix#L6-L18
 {
-  callPackage,
+  vivaldi,
   vivaldi-ffmpeg-codecs,
   widevine-cdm,
 }:
-let
-  vivaldi = callPackage ./vivaldi/package.nix { };
-in
 vivaldi.override {
   proprietaryCodecs = true;
   inherit vivaldi-ffmpeg-codecs;
