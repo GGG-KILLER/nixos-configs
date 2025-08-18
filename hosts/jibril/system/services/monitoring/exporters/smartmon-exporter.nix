aa1e366a938b11940d29199ae2be3a5e548e3ffe
GGG <gggkiller2@gmail.com>
2025-08-18 11:26:15 -0300

    jibril/monitoring: switch from self-made smartmon exporter to smartctl exporter.

--- hosts/jibril/system/services/monitoring/exporters/smartmon-exporter.nix
+++ hosts/jibril/system/services/monitoring/exporters/smartmon-exporter.nix
@@ -1,23 +1,23 @@
 { config, ... }:
 {
-  modules.services.smartmon-exporter.enable = true;
-  modules.services.smartmon-exporter.port = config.jibril.ports.prometheus-smartmontools-exporter;
+  services.prometheus.exporters.smartctl.enable = true;
+  services.prometheus.exporters.smartctl.listenAddress = "127.0.0.1";
+  services.prometheus.exporters.smartctl.port = config.jibril.ports.prometheus-smartmontools-exporter;
 
   services.prometheus.scrapeConfigs = [
     {
-      job_name = "smartmontools";
+      job_name = "smartctl";
       static_configs = [
         {
           targets = [
-            "${config.modules.services.smartmon-exporter.addr}:${toString config.modules.services.smartmon-exporter.port}"
+            "${config.services.prometheus.exporters.smartctl.listenAddress}:${toString config.services.prometheus.exporters.smartctl.port}"
           ];
           labels = {
             inherit (config.my.constants.prometheus) instance;
           };
         }
       ];
-      scrape_interval = "1m";
-      scrape_timeout = "50s";
+      inherit (config.my.constants.prometheus) scrape_interval;
     }
   ];
 }
