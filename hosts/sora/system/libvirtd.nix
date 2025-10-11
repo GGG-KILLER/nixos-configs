d8dbb2fe83fc6351bf5aa961329b32f3e6b0978b
GGG <gggkiller2@gmail.com>
2025-10-11 13:36:27 -0300

    sora/libvirtd: OVMF removal adaptation.

--- hosts/sora/system/libvirtd.nix
+++ hosts/sora/system/libvirtd.nix
@@ -1,18 +1,19 @@
-{ pkgs, ... }:
+{ ... }:
 {
   virtualisation.spiceUSBRedirection.enable = true;
 
   # libvirtd
   virtualisation.libvirtd.enable = true;
 
-  virtualisation.libvirtd.qemu.ovmf.enable = true;
-  virtualisation.libvirtd.qemu.ovmf.packages = [
-    (pkgs.OVMF.override {
-      secureBoot = true;
-      tpmSupport = true;
-      msVarsTemplate = true;
-    }).fd
-  ];
+  # TODO: check if OVMF removal needs any other updates.
+  # virtualisation.libvirtd.qemu.ovmf.enable = true;
+  # virtualisation.libvirtd.qemu.ovmf.packages = [
+  #   (pkgs.OVMF.override {
+  #     secureBoot = true;
+  #     tpmSupport = true;
+  #     msVarsTemplate = true;
+  #   }).fd
+  # ];
 
   virtualisation.libvirtd.qemu.swtpm.enable = true;
 }
