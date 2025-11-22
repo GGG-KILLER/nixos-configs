a5457d914375e98c97ce975cab39237bdfb44ab3
GGG <gggkiller2@gmail.com>
2025-11-22 11:17:04 -0300

    sora/desktop: temp fix for orca

--- hosts/sora/system/desktop/default.nix
+++ hosts/sora/system/desktop/default.nix
@@ -2,6 +2,7 @@
   self,
   inputs,
   system,
+  lib,
   pkgs,
   ...
 }:
@@ -120,4 +121,7 @@ in
   programs.winbox.enable = true;
   programs.winbox.package = self.packages.${system}.winbox4;
   programs.winbox.openFirewall = true;
+
+  # HACK: Temporary workaround for NixOS/nixpkgs#462935
+  systemd.user.services.orca.wantedBy = lib.mkForce [ ];
 }
