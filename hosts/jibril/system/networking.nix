aee7de65ff27064d6d667773421ffe93d274fcb5
GGG <gggkiller2@gmail.com>
2026-01-24 05:52:02 -0300

    jibril/networking: simplify

--- hosts/jibril/system/networking.nix
+++ hosts/jibril/system/networking.nix
@@ -1,110 +1,31 @@
 { lib, config, ... }:
-let
-  inherit (lib)
-    mkOption
-    types
-    mapAttrs
-    listToAttrs
-    nameValuePair
-    attrValues
-    removeSuffix
-    filter
-    ;
-  portOptions = {
-    options = {
-      protocol = mkOption {
-        type = types.enum [
-          "http"
-          "tcp"
-          "udp"
-        ];
-      };
-      port = mkOption {
-        type = types.port;
-        description = "the port number";
-      };
-      description = mkOption {
-        type = with types; nullOr str;
-        default = null;
-        description = "the port's description";
-      };
-    };
-  };
-  networkingOptions =
-    { name, ... }:
-    {
-      options = {
-        name = mkOption {
-          type = with types; str;
-          default = name;
-          description = "the name of this machine";
-        };
-        extraNames = mkOption {
-          type = with types; listOf str;
-          default = [ ];
-          description = "extra hostnames for this machine";
-        };
-        mainAddr = mkOption {
-          type = types.str;
-          description = "the main IP address of this machine";
-        };
-        extraAddrs = mkOption {
-          type = with types; attrsOf str;
-          description = "extra IP addresses that may be used to reach this machine";
-        };
-        ports = mkOption {
-          type = with types; listOf (submodule portOptions);
-          default = [ ];
-          description = "the ports used by the machine";
-        };
-      };
-    };
-in
 {
-  options.my.networking = mkOption { type = with types; attrsOf (submodule networkingOptions); };
+  networking = {
+    useDHCP = false;
+    hostName = "jibril";
+    hostId = "023937b5";
 
-  config = rec {
-    my.networking.jibril = {
-      mainAddr = config.home.addrs.jibril;
-      extraNames =
-        [ ]
-        ++ (map (name: removeSuffix ".lan" name) (
-          filter (name: removeSuffix ".lan" name != name) (
-            builtins.attrNames config.services.nginx.virtualHosts
-          )
-        ));
+    defaultGateway = config.home.addrs.router;
+    nameservers = [ config.home.addrs.router ];
+
+    interfaces.enp0s31f6 = {
+      ipv4.addresses = [
+        {
+          address = config.home.addrs.jibril;
+          prefixLength = 16;
+        }
+      ];
+      wakeOnLan.enable = true;
+      wakeOnLan.policy = [
+        "magic"
+        "broadcast"
+      ];
     };
 
-    networking = {
-      useDHCP = false;
-      hostName = "jibril";
-      hostId = "023937b5";
-
-      defaultGateway = config.home.addrs.router;
-      nameservers = [ config.home.addrs.router ];
-
-      interfaces.enp0s31f6 = {
-        ipv4.addresses = [
-          {
-            address = my.networking.jibril.mainAddr;
-            prefixLength = 16;
-          }
-        ];
-        wakeOnLan.enable = true;
-        wakeOnLan.policy = [
-          "magic"
-          "broadcast"
-        ];
-      };
-
-      hosts =
-        let
-          networking = mapAttrs (
-            netName: netCfg: netCfg // { names = [ netCfg.name ] ++ netCfg.extraNames; }
-          ) config.my.networking;
-          hostToNameValPair = host: nameValuePair host.mainAddr (map (name: "${name}.lan") host.names);
-        in
-        listToAttrs (map hostToNameValPair (attrValues networking));
-    };
+    hosts."127.0.0.1" = (
+      lib.filter (name: lib.removeSuffix ".lan" name != name) (
+        lib.attrNames config.services.caddy.virtualHosts
+      )
+    );
   };
 }
