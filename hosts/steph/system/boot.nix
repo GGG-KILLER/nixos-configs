f7ba7aed3f0dd9e3f39867834ca95cf674793b0f
GGG <github@ggg.dev>
2026-02-07 01:36:01 -0300

    steph: explicitly add Windows to boot menu on top

--- hosts/steph/system/boot.nix
+++ hosts/steph/system/boot.nix
@@ -1,12 +1,17 @@
-{ lib, ... }:
+{ ... }:
 {
   # Bootloader.
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.editor = false;
   boot.loader.systemd-boot.memtest86.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.windows."11" = {
+    title = "Windows 11";
+    efiDeviceHandle = "HD0a0a1";
+  };
 
   # Boot Order
+  boot.loader.systemd-boot.windows."11".sortKey = "x_windows";
   boot.loader.systemd-boot.sortKey = "y_nixos"; # make nixos 2nd to last
   boot.loader.systemd-boot.memtest86.sortKey = "z_memtest86"; # make memtest86+ last
 }
