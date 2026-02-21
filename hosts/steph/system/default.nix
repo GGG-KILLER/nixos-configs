7c3eebe19612f0e9b0ae02b4d3104ea527432663
GGG <github@ggg.dev>
2026-02-20 21:58:05 -0300

    treewide: xorg package set renaming

--- hosts/steph/system/default.nix
+++ hosts/steph/system/default.nix
@@ -18,12 +18,12 @@
     glibc # libdl
     gtk3 # libglib-2.0.so.0 libgobject-2.0.so.0 libgtk-3.so.0 libgdk-3.so.0
     libGL # libGL.so.1
-    xorg.libICE # libICE.so.6
-    xorg.libSM # libSM.so.6
-    xorg.libX11 # libX11 libX11.so.6
-    xorg.libXcursor # libXcursor.so.1
-    xorg.libXi # libXi.so.6
-    xorg.libXrandr # libXrandr.so.2
+    libice # libICE.so.6
+    libsm # libSM.so.6
+    libx11 # libX11 libX11.so.6
+    libxcursor # libXcursor.so.1
+    libxi # libXi.so.6
+    libxrandr # libXrandr.so.2
     fontconfig # libfontconfig.so.1
   ];
 
