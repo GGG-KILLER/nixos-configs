a2e6457aa4442f0ac18f5b9caa942f19309470da
GGG <github@ggg.dev>
2026-03-15 20:20:04 -0300

    modules/desktop/profile: remove sudoers extra rules
    They were broken and didn't allow for a full switch without typing in the password (which is actually dangerous and I don't know what I was thinking).

--- modules/desktop/profile.nix
+++ modules/desktop/profile.nix
@@ -1,9 +1,4 @@
-{
-  self,
-  lib,
-  config,
-  ...
-}:
+{ self, ... }:
 {
   imports = with self.nixosModules; [
     common-programs
@@ -29,32 +24,4 @@
   boot.kernel.sysctl = {
     "kernel.task_delayacct" = 1;
   };
-
-  security.sudo-rs.extraRules = [
-    # Allow to switch configurations through execution of
-    # "/nix/store/*/bin/switch-to-configuration" by users
-    # `ggg`, `root` without a password.
-    {
-      users = [
-        "ggg"
-        "root"
-      ];
-      commands = [
-        {
-          command = "/nix/store/*/bin/switch-to-configuration";
-          options = [
-            "SETENV"
-            "NOPASSWD"
-          ];
-        }
-        {
-          command = "${lib.getExe config.nix.package} build --no-link --profile /nix/var/nix/profiles/system /nix/store/*-nixos-system-*";
-          options = [
-            "SETENV"
-            "NOPASSWD"
-          ];
-        }
-      ];
-    }
-  ];
 }
