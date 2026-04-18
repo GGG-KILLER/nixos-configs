7f2e5cfa72bb5730b13f2aef92cac3b3dcc4c516
GGG <github@ggg.dev>
2026-04-18 02:24:46 -0300

    steph: switch from podman to docker

--- hosts/steph/system/services/default.nix
+++ hosts/steph/system/services/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./nix.nix
@@ -6,11 +6,18 @@
   ];
 
   # Docker
-  virtualisation.podman.enable = true;
-  virtualisation.podman.dockerCompat = true;
-  virtualisation.podman.autoPrune.enable = true;
-  virtualisation.podman.autoPrune.dates = "daily";
-  virtualisation.podman.autoPrune.flags = [ "--all" ];
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = false;
+  virtualisation.docker.daemon.settings = {
+    default-address-pools = lib.genList (x: {
+      base = "172.${toString (16 + x)}.0.0/16";
+      size = 24;
+    }) (31 - 16 + 1);
+  };
+  virtualisation.docker.autoPrune.enable = true;
+  virtualisation.docker.autoPrune.persistent = true;
+  virtualisation.docker.autoPrune.dates = "22:00";
+  virtualisation.docker.autoPrune.flags = [ "--all" ];
 
   # # VPN
   # services.mullvad-vpn.enable = true;
