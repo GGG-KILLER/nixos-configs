7822da33180f98a32412191e9195651154663f69
GGG <github@ggg.dev>
2026-04-30 21:37:41 -0300

    steph: remove Rider hack

--- hosts/steph/system/desktop/default.nix
+++ hosts/steph/system/desktop/default.nix
@@ -24,38 +24,39 @@ in
     android-tools
 
     # Coding
-    (
-      # HACK: early NixOS/nixpkgs#506159 but with a newer version
-      assert jetbrains.rider.version == "2025.3.3";
-      jetbrains.rider.overrideAttrs (old: {
-        src = fetchurl {
-          url = "https://download.jetbrains.com/rider/JetBrains.Rider-2026.1.0.1.tar.gz";
-          hash = "sha256-moIysTTsq7abpQfNh1Bc5Pk6VQgJIT6erbyHsUXf15Y=";
-        };
+    jetbrains.rider
+    # (
+    #   # HACK: early NixOS/nixpkgs#506159 but with a newer version
+    #   assert jetbrains.rider.version == "2025.3.3";
+    #   jetbrains.rider.overrideAttrs (old: {
+    #     src = fetchurl {
+    #       url = "https://download.jetbrains.com/rider/JetBrains.Rider-2026.1.0.1.tar.gz";
+    #       hash = "sha256-moIysTTsq7abpQfNh1Bc5Pk6VQgJIT6erbyHsUXf15Y=";
+    #     };
 
-        version = "2026.1.0.1";
-        buildNumber = "261.22158.394";
+    #     version = "2026.1.0.1";
+    #     buildNumber = "261.22158.394";
 
-        postInstall = ''
-          ls -d \
-            $out/*/bin/*/linux/*/lib/liblldb.so \
-            $out/*/bin/*/linux/*/lib/python3.*/lib-dynload/* \
-            $out/*/plugins/*/bin/*/linux/*/lib/liblldb.so \
-            $out/*/plugins/*/bin/*/linux/*/lib/python3.*/lib-dynload/* |
-          xargs patchelf \
-            --replace-needed libssl.so.10 libssl.so \
-            --replace-needed libssl.so.1.1 libssl.so \
-            --replace-needed libcrypto.so.10 libcrypto.so \
-            --replace-needed libcrypto.so.1.1 libcrypto.so \
-            --replace-needed libcrypt.so.1 libcrypt.so \
+    #     postInstall = ''
+    #       ls -d \
+    #         $out/*/bin/*/linux/*/lib/liblldb.so \
+    #         $out/*/bin/*/linux/*/lib/python3.*/lib-dynload/* \
+    #         $out/*/plugins/*/bin/*/linux/*/lib/liblldb.so \
+    #         $out/*/plugins/*/bin/*/linux/*/lib/python3.*/lib-dynload/* |
+    #       xargs patchelf \
+    #         --replace-needed libssl.so.10 libssl.so \
+    #         --replace-needed libssl.so.1.1 libssl.so \
+    #         --replace-needed libcrypto.so.10 libcrypto.so \
+    #         --replace-needed libcrypto.so.1.1 libcrypto.so \
+    #         --replace-needed libcrypt.so.1 libcrypt.so \
 
-          for dir in $out/rider/lib/ReSharperHost/linux-*; do
-            rm -rf $dir/dotnet
-            ln -s ${self.packages.${system}.combined-dotnet-sdk} $dir/dotnet
-          done
-        '';
-      })
-    )
+    #       for dir in $out/rider/lib/ReSharperHost/linux-*; do
+    #         rm -rf $dir/dotnet
+    #         ln -s ${self.packages.${system}.combined-dotnet-sdk} $dir/dotnet
+    #       done
+    #     '';
+    #   })
+    # )
     mockoon
 
     # Encryption
