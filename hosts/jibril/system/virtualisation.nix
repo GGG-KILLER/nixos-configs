a7ac4a402a238e18bbf9983628c762a75085642f
GGG <github@ggg.dev>
2026-05-20 14:25:39 -0300

    jibril/docker: use smaller subnets

--- hosts/jibril/system/virtualisation.nix
+++ hosts/jibril/system/virtualisation.nix
@@ -12,5 +12,19 @@
       persistent = true;
       randomizedDelaySec = "45min";
     };
+    daemon.settings = {
+      default-address-pools = [
+        # 4096 subnets with 256 usable hosts per subnet
+        {
+          base = "172.17.0.0/12";
+          size = 24;
+        }
+        # 4096 subnets with 14 usable hosts per subnet
+        {
+          base = "192.168.0.0/16";
+          size = 28;
+        }
+      ];
+    };
   };
 }
