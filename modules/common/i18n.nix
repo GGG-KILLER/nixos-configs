947af7790ba8a51ca0caa45c6fbe655cd952344b
GGG <github@ggg.dev>
2026-06-04 14:58:40 -0300

    common/i18n: fix locale issues

--- modules/common/i18n.nix
+++ modules/common/i18n.nix
@@ -6,11 +6,10 @@
   };
   # I18n Settings
   i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
-
-  i18n.supportedLocales = lib.mkDefault [
-    "en_US.UTF-8/UTF-8"
-    "pt_BR.UTF-8/UTF-8"
-  ];
+  i18n.extraLocales = lib.mkDefault [ "pt_BR.UTF-8/UTF-8" ];
+  i18n.extraLocaleSettings = lib.listToAttrs (
+    lib.map (name: lib.nameValuePair name "pt_BR.UTF-8") [ "LC_ADDRESS" "LC_MEASUREMENT" "LC_NAME" "LC_PAPER" "LC_TELEPHONE" "LC_TIME" ]
+  );
 
   # Timezone
   time.timeZone = lib.mkDefault "America/Sao_Paulo";
